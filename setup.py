f85df57c1ec3267d11286ef4efd696c8eeb54d48
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-07-29 20:30:15 +0000

    initial checkin
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@44701 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ setup.py
@@ -0,0 +1,42 @@
+import codecs
+from setuptools import setup, find_packages
+
+VERSION = '0.0.0'
+
+entry_points = {
+    "z3c.autoinclude.plugin": [
+		'target = nti.app',
+	],
+}
+
+import platform
+py_impl = getattr(platform, 'python_implementation', lambda: None)
+IS_PYPY = py_impl() == 'PyPy'
+
+setup(
+    name='nti.contenttools',
+    version=VERSION,
+    author='Jason Madden',
+    author_email='jason@nextthought.com',
+    description="NTI Content Tools",
+    long_description=codecs.open('README.rst', encoding='utf-8').read(),
+    license='Proprietary',
+    keywords='pyramid preference',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+		'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        ],
+	packages=find_packages('src'),
+	package_dir={'': 'src'},
+	namespace_packages=['nti','nti.app'],
+	install_requires=[
+		'setuptools',
+        'nti.dataserver',
+	],
+	entry_points=entry_points
+)
