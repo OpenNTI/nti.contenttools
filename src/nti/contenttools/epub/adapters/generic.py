8e0cbe7bc62851e7f7e200c6fe35ac11f7e46f21
Egawati Panjei <egawati.panjei@nextthought.com>
2014-07-31 21:14:36 +0000

    add generic adapter >>  still need to add tag for tex
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@44905 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/contenttools/epub/adapters/generic.py
@@ -0,0 +1,512 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+import os
+try:
+    import cStringIO as StringIO
+except:
+    import StringIO
+
+from ... import types
+
+from PIL import Image as PILImage
+from urlparse import urlparse
+
+from lxml import etree
+
+
+class Chapter( types.Chapter ):
+
+    @classmethod
+    def process(cls, element, epub):
+        me = cls()
+        me.add_child( Run.process(element, epub) )
+        return me
+
+
+class Section( types.Section ):
+
+    @classmethod
+    def process(cls, element, epub):
+        me = cls()
+        me.add_child( Run.process(element, epub) )
+        return me
+
+
+class SubSection( types.SubSection ):
+
+    @classmethod
+    def process(cls, element, epub):
+        me = cls()
+        me.add_child( Run.process(element, epub) )
+        return me
+
+
+class SubSubSection( types.SubSubSection ):
+
+    @classmethod
+    def process(cls, element, epub):
+        me = cls()
+        me.add_child( Run.process(element, epub) )
+        return me
+
+
+class Paragraph( types.Paragraph ):
+
+    @classmethod
+    def process(cls, element, epub):
+        me = cls()
+
+        if 'id' in element.attrib:
+            print ('id is here element.attrib')
+            me.add_child( Label.process( element, epub ) )
+
+        print('Class Paragraph element.text type: ', type(element.text))    
+
+        if element.text:
+            print(element.text)
+            me.add_child( types.TextNode( element.text ) )
+
+        for child in element:
+            if child.tag == 'a':
+                me.add_child( _process_a_elements( child, epub ) )
+            elif child.tag == 'b':
+                me.add_child( _process_b_elements( child, epub ) )
+            elif child.tag == 'i':
+                me.add_child( _process_i_elements( child, epub ) )
+            elif child.tag == 'img':
+                me.add_child( Image.process( child, epub ) )
+            elif child.tag == 'p':
+                me.add_child( _process_p_elements( child, epub ) )
+            elif child.tag == 'span':
+                me.add_child( _process_span_elements( child, epub ) )
+            elif child.tag == 'sub':
+                me.add_child( _process_sub_elements( child, epub ) )
+            elif child.tag == 'video':
+                me.add_child( Video.process( child, epub ) )
+            else:
+                print('Unhandled p child: %s' % child)
+
+        if element.tail:
+            me.add_child( types.TextNode( element.tail.replace('\r', '' ) ) )
+
+        return me
+
+
+class Run( types.Run ):
+
+    @classmethod
+    def process(cls, element, epub, styles=[]):
+        me = cls()
+        me.styles.extend(styles)
+        print('at generic.py: class Run : element.text >>',element.text)
+        if element.text:
+            #print('element.text IS NOT EMPTY :',element.text, '<<')
+            me.add_child( types.TextNode( element.text ) )
+
+        for child in element:
+            if child.tag == 'a':
+                print("FOUND child.tag == 'a'")
+                me.add_child( _process_a_elements( child, epub ) )
+            elif child.tag == 'b':
+                print("FOUND child.tag == 'b'")
+                me.add_child( _process_b_elements( child, epub ) )
+            elif child.tag == 'br':
+                print("FOUND child.tag == 'br'")
+                me.add_child( types.Newline() )
+                me.add_child( types.TextNode( child.tail ) )
+            elif child.tag == 'i':
+                print("FOUND child.tag == 'i'")
+                me.add_child( _process_i_elements( child, epub ) )
+            elif child.tag == 'span':
+                print("FOUND child.tag == 'span'")
+                me.add_child( _process_span_elements( child, epub ) )
+            elif child.tag == 'sub':
+                print("FOUND child.tag == 'sub'")
+                me.add_child( _process_sub_elements( child, epub ) )
+            elif child.tag == 'p':
+                print("FOUND child.tag == 'p'")
+                me.add_child(_process_p_elements( child, epub ))
+            elif child.tag == 'div':
+                print("FOUND child.tag == 'div'")
+                me.add_child(_process_div_elements(child, epub))
+            elif child.tag == 'h2':
+                print("FOUND child.tag == 'h2'")
+                me.add_child(_process_h2_elements(child, epub ))
+            elif child.tag == 'section':
+                print("FOUND child.tag == 'section'")
+                me.add_child(_process_section_elements(child, epub))
+            else:
+                print('Unhandled Run child: %s' % child)
+
+        if element.tail:
+            #print("check if element.tail:", element.tail)
+            _t = cls()
+            _t.add_child( me )
+            _t.add_child( types.TextNode( element.tail.replace('\r', '' ) ) )
+            me = _t
+
+        return me
+
+
+class Hyperlink( types.Hyperlink ):
+
+    def __init__(self):
+        self.type = 'Normal'
+
+    @classmethod
+    def process(cls, link, epub ):
+        me = cls()
+
+        me.target = link.attrib['href']
+
+        if link.text:
+            me.add_child( types.TextNode( link.text ) )
+
+        for child in link:
+            me.add_child( Run.process( child, epub ) )
+
+        return me
+
+class Label( types.Label ):
+
+    @classmethod
+    def process(cls, label, epub ):
+        me = cls()
+        me.name = label.attrib['id']
+        return me
+
+
+class Sidebar( types.Sidebar ):
+
+    @classmethod
+    def process(cls, element, epub):
+        me = cls()
+
+        for child in element:
+            if child.tag == 'div':
+                el = _process_div_elements( child, epub )
+                if 'class' in child.attrib.keys() and 'title' in child.attrib['class']:
+                    me.title = el
+                else:
+                    me.add_child( el )
+            elif child.tag == 'p':
+                me.add_child( _process_p_elements( child, epub ) )
+            elif child.tag == 'ul':
+                me.add_child( _process_ul_elements( child, epub ) )
+            else:
+                print('Unhandled Sidebar child: %s' % child)
+
+        return me
+
+class BlockQuote( types.BlockQuote ):
+
+    @classmethod
+    def process(cls, element, epub):
+        me = cls()
+        me.add_child( Paragraph.process( element, epub ) )
+        return me
+
+class Image( types.Image ):
+
+    @classmethod
+    def process(cls, element, epub):
+        me = cls()
+        me.path = element.attrib['src']
+        me.caption = element.attrib['alt']
+        me.data = StringIO.StringIO( epub.zipfile.read(os.path.join(epub.content_path, me.path)) )
+        me.width, me.height = PILImage.open(me.data).size
+        epub.image_list.append(me)
+        return me
+
+class Video( types.Video ):
+
+    @classmethod
+    def process(cls, element, epub):
+        me = cls()
+        me.path = element.attrib['src']
+        me.caption = element.attrib['title']
+        me.thumbnail = element.attrib['poster']
+        me.thumbnaildata = StringIO.StringIO( epub.zipfile.read(os.path.join(epub.content_path, me.thumbnail)) )
+        #me.width, me.height = PILImage.open(me.thumbnaildata).size
+        me.width, me.height = ( 640, 480 )
+        me.add_child( Run.process( element, epub ) )
+        epub.video_list.append(me)
+        return me
+
+class UnorderedList( types.UnorderedList ):
+
+    @classmethod
+    def process(cls, element, epub):
+        me = cls()
+
+        for child in element:
+            el = None
+            if child.tag == 'li':
+                el = Item.process(child, epub)
+            elif child.tag == 'div':
+                el = _process_div_elements(child, epub)
+            elif child.tag == 'p':
+                el = _process_p_elements(child, epub)
+            else:
+                print('undordered list',child)
+                el = Item()
+
+            if isinstance(el, types.Item) or isinstance(el, types.List):
+                me.add_child( el )
+            else:
+                if len(me.children) == 0:
+                    me.add_child( Item() )
+                me.children[-1].add_child( el )
+
+        return me
+
+
+class Item( types.Item ):
+
+    @classmethod
+    def process(cls, element, epub):
+        me = None
+        child = Run.process(element, epub)
+        if isinstance(child, types.List):
+            me = child
+        else:
+            me = cls()
+            me.add_child( child )
+        return me
+
+
+def adapt( fragment, epub, label ):
+    els = _process_fragment( fragment, epub )
+
+    # Make sure that each chapter and section object have a Label child
+    if els and (isinstance(els[0], Chapter) or isinstance(els[0], Section)):
+        label_el = Label()
+        label_el.name = label
+        els[0].add_child( label_el )
+
+    # Upgrade front and back matter 'sections' to 'chapters' as appropriate
+    if els and isinstance(els[0], Section):
+        if els[0].children[-1].name[0] in [ 'b', 'f' ] and len(els[0].children[-1].name) == 3:
+            chapter = Chapter()
+            for child in els[0].children:
+                chapter.add_child(child)
+            els[0] = chapter
+
+    for el in els:
+        _fix_hyperlinks( el, epub.manifest )
+
+    return els
+
+def _fix_hyperlinks( element, manifest ):
+
+    if isinstance( element, Hyperlink ):
+        hyperparts = urlparse( element.target )
+        for item in manifest:
+            if hyperparts[2] == manifest[item]['href']:
+                element.type = u'Pageref'
+                if hyperparts[5]:
+                    element.target = hyperparts[5]
+                else:
+                    element.target = item
+    else:
+        if hasattr(element, 'children'):
+            for child in element.children:
+                _fix_hyperlinks( child, manifest )
+
+def _process_fragment( fragment, epub ):
+    head = fragment.find('head')
+    body = fragment.find('body')
+    el = []
+    for element in body:
+        print('^^^^^^^^^^')
+        print('element tag: ',element.tag,'-type of element in body : ', type(element))
+        if element.tag == 'div':
+            el.append(_process_div_elements( element, epub ))
+        elif element.tag == 'p':
+            el.append(_process_p_elements( element, epub ))
+        elif element.tag == 'h1':
+            el.append(_process_h1_elements( element, epub ))
+        elif element.tag == 'h2':
+            el.append(_process_h2_elements( element, epub ))
+        elif element.tag == 'ul':
+            el.append(_process_ul_elements( element, epub ))
+        elif element.tag == 'section':
+            el.append(_process_section_elements(element, epub))
+        else:
+            print('Unhandled body child: %s >> %s',element.tag, element )
+        print('**********')
+    # Consolidate multi-line chapter or section titles
+    new_el = []
+    for i in xrange(len(el)):
+        if (i+1 < len(el)) and type(el[i]) == type(el[i+1]):
+            if type(el[i]) == Chapter or type(el[i]) == Section:
+                el[i].add_child( types.TextNode(': ') )
+                for child in el[i+1].children:
+                    el[i].add_child( child )
+                el[i+1] = el[i]
+            else:
+                new_el.append(el[i])
+        else:
+            new_el.append(el[i])
+    el = new_el
+    el2 = el
+    # If the chapter or section title was not parsed out of the text, then extract it from the document head.
+    # Maybe we should do this all of the time.
+    chapter = Chapter()
+    chapter.suppressed = True
+    print('_get_title(head) >> ', _get_title( head ))
+    chapter.add_child( types.TextNode( _get_title( head ) ) )
+    print (el)
+    if el is []:
+        print ('append el')
+        el.append(chapter)
+    elif not ( (isinstance(el[0], Chapter) or isinstance(el[0], Section)) ):
+        print('el.insert works')
+        el.insert(0,chapter)
+
+    # Consolidate list elements
+    el3 = el
+    eldiff = list(set(el3)-set(el2))
+    ellcommon = list(set(el3) & set(el2))
+    print('eldiff: ', eldiff)
+    print('ellcommon: ', ellcommon)
+    el = _consolidate_lists( el )
+
+    return el
+
+def _get_title( head ):
+    title = u''
+    for element in head:
+        if element.tag == 'title':
+            title = element.text
+    return title
+
+def _consolidate_lists( list = [] ):
+    def pull_up_children( element ):
+        if isinstance(element, types.List):
+            for i in xrange(len(element.children)):
+                if isinstance(element.children[i], types.List) and element.level == element.children[i].level:
+                    child = element.children[i]
+                    for j in xrange(len(child.children)):
+                        element.children.insert(i+j, child.children[j])
+                    element.children.remove(child)
+
+    new_list = []
+    for i in range(len(list)):
+        if isinstance(list[i], types.List) and (i + 1 < len(list)) and isinstance(list[i+1], types.List) and list[i].group == list[i+1].group:
+            if list[i].level == list[i+1].level:
+                for child in list[i+1].children:
+                    list[i].add_child( child )
+                    list[i+1] = list[i]
+            elif list[i].level < list[i+1].level:
+                list[i].add_child( list[i+1] )
+                list[i+1] = list[i]
+            else:
+                list[i].children = _consolidate_lists( list[i].children )
+                # Pull up children if necessary
+                pull_up_children( list[i] )
+                new_list.append( list[i] )
+        else:
+            if isinstance(list[i], types.List):
+                list[i].children = _consolidate_lists( list[i].children )
+                # Pull up children if necessary
+                pull_up_children( list[i] )
+            new_list.append( list[i] )
+    return new_list
+
+def _process_div_elements( element, epub ):
+    print ('check element atrribute of body:', element.attrib.keys())
+    #print ('type: ',type(element))
+    class_ = u''
+    el = None
+    if 'class' in element.attrib.keys():
+        class_ = element.attrib['class']
+        print ('div: check class_', class_)
+        el = Run.process( element, epub )
+    elif 'id' in element.attrib.keys() and el == None:
+        el = Run.process( element, epub )
+    else:
+        el = Paragraph.process(element,epub)
+    return el
+
+
+def _process_p_elements( element, epub ):
+    print ('check element atrribute of body:P', element.attrib.keys())
+    #print ('type: ',type(element))
+    class_ = u''
+    el = None
+    if 'class' in element.attrib.keys():
+        class_ = element.attrib['class']
+        print ('div: check class_', class_)
+        el = Run.process( element, epub )
+    elif 'id' in element.attrib.keys() and el == None:
+        el = Run.process( element, epub )
+    else:
+        el = Paragraph.process(element,epub)
+    return el
+
+def _process_section_elements(element, epub):
+    print ('check element atrribute of body:SECTION', element.attrib.keys())
+    class_ = u''
+    el = None
+    if 'class' in element.attrib.keys():
+        class_ = element.attrib['class']
+        print ('div: check class_', class_)
+        el = Run.process( element, epub )
+    elif 'id' in element.attrib.keys() and el == None:
+        el = Run.process( element, epub )
+    else:
+        el = Section.process(element,epub)
+    return el
+
+def _process_h1_elements( element, epub ):
+    # Model as a subsection
+    return SubSection.process(element, epub)
+
+def _process_h2_elements( element, epub ):
+    # Model as a subsubsection
+    return SubSubSection.process(element, epub)
+
+def _process_span_elements( element, epub ):
+    return Run.process(element, epub)
+
+def _process_ul_elements( element, epub ):
+    return UnorderedList.process(element, epub)
+
+def _process_b_elements( element, epub ):
+    return Run.process(element, epub, ['bold'])
+
+def _process_i_elements( element, epub ):
+    return Run.process(element, epub, ['italic'])
+
+def _process_sub_elements( element, epub ):
+    return Run.process(element, epub, ['sub'])
+
+def _process_a_elements( element, epub ):
+    if 'href' in element.attrib.keys():
+        el = None
+        if element.tail:
+            el = Run()
+            el.add_child( Hyperlink.process(element, epub) )
+            el.add_child( types.TextNode(element.tail) )
+        else:
+            el = Hyperlink.process(element, epub)
+        return el
+    else:
+        el = None
+        if element.tail:
+            el = Run()
+            el.add_child( Label.process(element, epub) )
+            el.add_child( types.TextNode(element.tail) )
+        else:
+            el = Label.process(element, epub)
+        return el
+
