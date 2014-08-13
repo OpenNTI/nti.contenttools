a2769a482589e08b570e576bb42d0df3168d0404
Egawati Panjei <egawati.panjei@nextthought.com>
2014-08-13 21:12:45 +0000

    add unittest to check whether contentfragments.latex read the extended _escape list declared in contenttools.latex --> run test failed
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@46469 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/contenttools/tests/test_extended_escape_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+import unittest
+
+from nti.contenttools import latex
+from nti.contentfragments.interfaces import PlainTextContentFragment
+from nti.contentfragments.latex import PlainTextToLatexFragmentConverter
+
+class TestExtendedEscapeList(unittest.TestCase):
+	def test_extended_unit_test(self):
+		plain_text = u"hello from plain µ"
+		latex_tex  = u'hello from plain $\\mu$'
+		result_tex = PlainTextToLatexFragmentConverter(plain_text, text_scaper='extended')
+		self.assertEqual(latex_tex,result_tex)
+
+
+		plain_text = u"hello from plain δ Τ"
+		latex_tex  = u'hello from plain $\\delta$ $\\Tau$'
+		result_tex = PlainTextToLatexFragmentConverter(plain_text, text_scaper='extended')
+		self.assertEqual(latex_tex,result_tex)
