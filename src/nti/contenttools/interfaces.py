8d1e1b440271efbf76a61ea20cc7bed0e12749bf
Egawati Panjei <egawati.panjei@nextthought.com>
2014-08-13 16:50:44 +0000

    add utility for math latex tag
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@46413 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/contenttools/interfaces.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id: interfaces.py 44327 2014-07-27 06:48:14Z carlos.sanchez $
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+from zope import interface
+
+class IMathLatexEscaper(interface.Interface):
+	pass
+
