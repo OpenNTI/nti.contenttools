57a4929b3d1ecb03d9f2fe8eecd9f2be26a2ee9b
Egawati Panjei <egawati.panjei@nextthought.com>
2014-08-13 21:30:19 +0000

    rename method in test_extended_escape list and add a new char to test
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@46473 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttools/tests/test_extended_escape_list.py
+++ src/nti/contenttools/tests/test_extended_escape_list.py
@@ -11,14 +11,20 @@ from nti.contentfragments.interfaces import PlainTextContentFragment
 from nti.contentfragments.latex import PlainTextToLatexFragmentConverter
 
 class TestExtendedEscapeList(unittest.TestCase):
-	def test_extended_unit_test(self):
+	def test_extended_escape_chars(self):
 		plain_text = u"hello from plain µ"
-		latex_tex  = u'hello from plain $\\mu$'
+		latex_tex  = u"hello from plain $\\mu$"
 		result_tex = PlainTextToLatexFragmentConverter(plain_text, text_scaper='extended')
 		self.assertEqual(latex_tex,result_tex)
 
+		plain_text = u"check arrow ↑"
+		latex_tex  = u"check arrow $\\uparrow$"
+		result_tex = PlainTextToLatexFragmentConverter(plain_text, text_scaper='extended')
+		self.assertEqual(latex_tex,result_tex)
 
 		plain_text = u"hello from plain δ Τ"
-		latex_tex  = u'hello from plain $\\delta$ $\\Tau$'
+		latex_tex  = u"hello from plain $\\delta$ $\\Tau$"
 		result_tex = PlainTextToLatexFragmentConverter(plain_text, text_scaper='extended')
 		self.assertEqual(latex_tex,result_tex)
+
+
