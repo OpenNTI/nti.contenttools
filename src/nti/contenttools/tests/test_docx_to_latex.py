a307d23275ec7db373bb435881b94e11a1f3d8e4
Egawati Panjei <egawati.panjei@nextthought.com>
2014-08-19 20:35:40 +0000

    add test docx to latex
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@47449 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/contenttools/tests/test_docx_to_latex.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+from hamcrest import is_
+from hamcrest import none
+from hamcrest import is_not
+from hamcrest import assert_that
+
+from zope import component
+
+from nti.contenttools.docx.read import DocxFile
+from nti.contenttools.word2latex import _title_escape
+from nti.contenttools.tests import ContentToolsTestCase
+
+import os
+import codecs
+
+
+
+class TestDocxToLatex(ContentToolsTestCase):
+	def test_convert_docx_to_latex(self):
+		inputfile = "src/nti/contenttools/tests/reference_sample.docx"
+		outputdir = 'src/nti/contenttools/tests/output/'
+		if not os.path.exists( outputdir ):
+			os.mkdir( outputdir )		
+		docxFile = DocxFile(inputfile)
+		if docxFile.title:
+			outputfile = os.path.join(outputdir, _title_escape(docxFile.title)+'.tex')
+		else:
+			outputfile = os.path.join(outputdir, _title_escape(os.path.splitext(os.path.basename(inputfile))[0])+'.tex')
+
+		testfile = 'src/nti/contenttools/tests/reference_sample.tex'
+		assert_that('src/nti/contenttools/tests/output/reference_sample.tex',is_(outputfile))
+
+		with codecs.open( outputfile, 'w', 'utf-8' ) as fp:
+			fp.write( docxFile.render() )
+		
+		latex_file = open(testfile, 'r')
+		result_file = open(outputfile, 'r')
+
+		assert_that(latex_file.read(), is_(result_file.read()))
