7d245f32d497f18f57cb8b814e58f7be950266a5
Egawati Panjei <egawati.panjei@nextthought.com>
2014-08-25 22:12:52 +0000

    docx-latex: put list of ignored_tags in a new module
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@48011 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/contenttools/docx/ignored_tags.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id: paragraph.py 47840 2014-08-22 20:41:57Z egawati.panjei $
+"""
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from . import properties as docx
+
+IGNORED_TAGS = [ '{'+docx.nsprefixes['w']+'}ind',
+		 '{'+docx.nsprefixes['w']+'}sectPr',
+		 '{'+docx.nsprefixes['w']+'}proofErr',
+		 '{'+docx.nsprefixes['w']+'}noProof',
+		 '{'+docx.nsprefixes['w']+'}commentReference',
+		 '{'+docx.nsprefixes['w']+'}commentRangeEnd',
+		 '{'+docx.nsprefixes['w']+'}commentRangeStart',
+		 '{'+docx.nsprefixes['w']+'}bookmarkEnd',
+		 '{'+docx.nsprefixes['w']+'}bookmarkStart',
+		 '{'+docx.nsprefixes['w']+'}shd',
+		 '{'+docx.nsprefixes['w']+'}contextualSpacing',
+		 '{'+docx.nsprefixes['w']+'}tabs',
+		 '{'+docx.nsprefixes['w']+'}highlight',
+		 '{'+docx.nsprefixes['w']+'}jc',
+		 '{'+docx.nsprefixes['w']+'}keepNext',
+		 '{'+docx.nsprefixes['w']+'}outlineLvl',
+		 '{'+docx.nsprefixes['w']+'}lastRenderedPageBreak',
+		 '{'+docx.nsprefixes['w']+'}shd',
+		 '{'+docx.nsprefixes['w']+'}spacing',
+		 '{'+docx.nsprefixes['w']+'}autoSpaceDE',
+		 '{'+docx.nsprefixes['w']+'}autoSpaceDN',
+		 '{'+docx.nsprefixes['w']+'}adjustRightInd',
+		 '{'+docx.nsprefixes['w']+'}tab',
+		 '{'+docx.nsprefixes['w']+'}rtl']
