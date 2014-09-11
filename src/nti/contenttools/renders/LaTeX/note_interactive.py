ea77ef8ec7d3be338d5c28a99adb173d591bf4c9
Egawati Panjei <egawati.panjei@nextthought.com>
2014-09-11 21:29:31 +0000

    epub-latex automate interactive note using nticard
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@49189 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/contenttools/renders/LaTeX/note_interactive.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id: note_interactive.py 48867 2014-09-08 17:27:39Z egawati.panjei $
+"""
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+def note_interactive_rendered(self):
+	new_image_path = u'images/%s' %(self.image_path)
+	return u'\n\\begin{nticard}{%s}<creator=Carpe Noctum>\n\\label{%s}\n\\caption{%s}\n\\includegraphics{%s}\n%s\n\\end{nticard}\n'\
+	 	%(self.link, self.label, self.caption, new_image_path, self.notes)
