f42a0685579c6c93ed13d71fb50c5b04c89db868
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-09-12 23:25:17 +0000

    set props
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@49320 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttools/epub/adapters/glossary.py
+++ src/nti/contenttools/epub/adapters/glossary.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-.. $Id: glossary.py 47366 2014-08-18 16:25:00Z egawati.panjei $
+.. $Id$
 """
 from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
--- src/nti/contenttools/epub/openstax_epub.py
+++ src/nti/contenttools/epub/openstax_epub.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-.. $Id: openstax_epub.py 49289 2014-09-12 17:49:09Z egawati.panjei $
+.. $Id$
 """
 from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
