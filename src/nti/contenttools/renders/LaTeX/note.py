4f7d921c258eb7b2cabb1e60581de2992d30db34
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-10-02 00:33:07 +0000

    set props
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@50332 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttools/renders/LaTeX/note.py
+++ src/nti/contenttools/renders/LaTeX/note.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-.. $Id: note.py 50214 2014-09-30 22:01:38Z egawati.panjei $
+.. $Id$
 """
 
 from __future__ import print_function, unicode_literals, absolute_import, division
@@ -9,16 +9,12 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-from IPython.core.debugger import Tracer
-
-from .base import base_renderer
-
 """
 module to render note found in each chapter of openstax epub
 """
 
 def openstax_note_renderer(self):
-	logger.info("found note sidebar")
+	logger.debug("found note sidebar")
 	title = self.title.render()
 	body = self.body.render()
 	return u'\n\\begin{sidebar}{%s}\n%s\n\\end{sidebar}\n' %(title, body)
