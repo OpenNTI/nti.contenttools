6ea22cd2d5a93204f0dbb28d4ce67cd2d95dc1ab
Egawati Panjei <egawati.panjei@nextthought.com>
2014-10-06 22:08:21 +0000

    openstax: make sure that each equation image will use includegraphics tag
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@50601 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttools/epub/adapters/equation_image.py
+++ src/nti/contenttools/epub/adapters/equation_image.py
@@ -13,6 +13,7 @@ logger = __import__('logging').getLogger(__name__)
 from lxml.html import HtmlComment
 
 from ... import types
+from IPython.core.debugger import Tracer
 
 class EquationImage(types.EquationImage):
 
@@ -25,8 +26,16 @@ class EquationImage(types.EquationImage):
 				me.label = Run.process(child, epub)
 			elif child.tag == 'div' and child.attrib['class'] == 'mediaobject':
 				me.image = Run.process(child, epub)
+				me.image = set_equation_image(me.image)
 			elif isinstance(child, HtmlComment):
 				pass
 			else:
 				me.text = Run.process(child, epub)
 		return me
+
+def set_equation_image(node):
+	for child in node:
+		if isinstance(child, types.Image):
+			child.equation_image = True
+	return node
+
