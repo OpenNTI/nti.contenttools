04f2d8f35accdcffffe106ad061d29c5de84d760
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-10-09 15:31:59 +0000

    use nti prefix for executable scripts
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@50796 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup, find_packages
 VERSION = '0.0.0'
 
 entry_points = {
-    "z3c.autoinclude.plugin": [
+	"z3c.autoinclude.plugin": [
 		'target = nti.dataserver',
 	],
-    'console_scripts': [
-        'import_docx = nti.contenttools.word2latex:main',
-        'import_epub = nti.contenttools.import_epub:main',
-    ]
+	'console_scripts': [
+		'nti_import_docx = nti.contenttools.word2latex:main',
+		'nti_import_epub = nti.contenttools.import_epub:main',
+	]
 }
 
 import platform
@@ -18,29 +18,29 @@ py_impl = getattr(platform, 'python_implementation', lambda: None)
 IS_PYPY = py_impl() == 'PyPy'
 
 setup(
-    name='nti.contenttools',
-    version=VERSION,
-    author='Jason Madden',
-    author_email='jason@nextthought.com',
-    description="NTI Content Tools",
-    long_description=codecs.open('README.rst', encoding='utf-8').read(),
-    license='Proprietary',
-    keywords='pyramid preference',
-    classifiers=[
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
+	name='nti.contenttools',
+	version=VERSION,
+	author='Jason Madden',
+	author_email='jason@nextthought.com',
+	description="NTI Content Tools",
+	long_description=codecs.open('README.rst', encoding='utf-8').read(),
+	license='Proprietary',
+	keywords='content tool',
+	classifiers=[
+		'Intended Audience :: Developers',
+		'Natural Language :: English',
+		'Operating System :: OS Independent',
 		'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        ],
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.3',
+		],
 	packages=find_packages('src'),
 	package_dir={'': 'src'},
 	namespace_packages=['nti'],
 	install_requires=[
 		'setuptools',
-        'nti.dataserver',
+		'nti.dataserver',
 	],
 	entry_points=entry_points
 )
