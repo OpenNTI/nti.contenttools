da9c11ae4ebdb239e34a0b74701c0a8e30910594
Egawati Panjei <egawati.panjei@nextthought.com>
2014-10-27 18:52:20 +0000

    openstax: when render note interactive (nticard element) we need to save the image_path to a file incase we need to change those images later
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@52291 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttools/renders/LaTeX/note_interactive.py
+++ src/nti/contenttools/renders/LaTeX/note_interactive.py
@@ -8,8 +8,15 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+import codecs
+import os
+import __builtin__
+
 def note_interactive_rendered(self):
 	new_image_path = u'images/%s' %(self.image_path)
+	#save image path infomation to file, in case content team wants to replace those images
+	with codecs.open(__builtin__.nticard_images_filename, 'a', 'utf-8') as fp:
+		fp.write("%s\n" %new_image_path)
 	caption = self.caption.render().rstrip()
 	return u'\n\\begin{nticard}{%s}\n\\label{%s}\n\\caption{%s}\n\\includegraphics{%s}\n%s\n\\end{nticard}\n'\
 	 	%(self.link, self.label, caption, new_image_path, self.notes)
