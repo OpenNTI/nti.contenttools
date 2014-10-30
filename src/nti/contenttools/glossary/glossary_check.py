75b6b857a4e8b777fed650bd544879fa1581a1b6
Egawati Panjei <egawati.panjei@nextthought.com>
2014-10-30 19:43:24 +0000

    add a new tool: glossary finder for latex file
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@52545 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttools/glossary/glossary_check.py
+++ src/nti/contenttools/glossary/glossary_check.py
@@ -13,52 +13,53 @@ import os
 import codecs
 import re
 
-def process_glossary(glossary_dict, filename):
+def process_glossary(glossary_dict, filename, search_text=None):
         content = get_file_content(filename)
         content = content.decode('utf-8')
         pattern = None
+
         for key in glossary_dict.keys():
                 key_text =key.rstrip()
                 definition = glossary_dict[key].rstrip()
-                content = add_definition(key_text, definition, content)
+                content = add_definition(key_text, definition, content, search_text)
 
 
                 #if the glossary term located in the beginning of a sentence
                 cap_key = key_text.capitalize()
-                content = add_definition(cap_key, definition, content)
+                content = add_definition(cap_key, definition, content, search_text)
 
                 #if the key use capital letter in the beginning of word 
                 #while glossary term found in the chapter use lower case
                 lower_key = capital_to_lower_case(key_text)
-                content = add_definition(lower_key, definition, content)
+                content = add_definition(lower_key, definition, content, search_text)
                 #incase lower key located in the beginning of a sentence
                 b_lower_key = lower_key.capitalize()
-                content = add_definition(b_lower_key, definition, content)
+                content = add_definition(b_lower_key, definition, content, search_text)
 
 
                 #if the glossary term has textit tag
                 italic_key = u'\\textit{%s}' %(key_text)
-                content = add_definition(italic_key, definition, content)
+                content = add_definition(italic_key, definition, content, search_text)
 
                 #if the glossary term is singular but listed as a plural word in the glossary dict
                 single_key = plural_to_single_word(key_text)
-                content = add_definition(single_key, definition, content)
+                content = add_definition(single_key, definition, content, search_text)
                 single_key = plural_to_single_word(lower_key)
-                content = add_definition(single_key, definition, content)
+                content = add_definition(single_key, definition, content, search_text)
                 single_key = plural_to_single_word(b_lower_key)
-                content = add_definition(single_key, definition, content)
+                content = add_definition(single_key, definition, content, search_text)
 
                 #if the glossary term is plural but listed as a single word in the glossary dict
                 plural_key = single_to_plural_word(key_text)
-                content = add_definition(plural_key, definition, content)
+                content = add_definition(plural_key, definition, content, search_text)
                 plural_key = single_to_plural_word(cap_key)
-                content = add_definition(plural_key, definition, content)
+                content = add_definition(plural_key, definition, content, search_text)
                 plural_key = single_to_plural_word(italic_key)
-                content = add_definition(plural_key, definition, content)
+                content = add_definition(plural_key, definition, content, search_text)
                 plural_key = single_to_plural_word(lower_key)
-                content = add_definition(plural_key, definition, content)
+                content = add_definition(plural_key, definition, content, search_text)
                 plural_key = single_to_plural_word(b_lower_key)
-                content = add_definition(plural_key, definition, content)
+                content = add_definition(plural_key, definition, content, search_text)
 
 
         new_content = content
@@ -95,9 +96,18 @@ def plural_to_single_word(word):
         else:
                 return word
 
-def add_definition(key, definition, content):
-        search_text = u'\\ntiglossaryentry{\\textbf{%s}}{definition}' % (key)
-        new_text = u'\\ntiglossaryentry{\\textbf{%s}}{%s}' % (key, definition)
+def add_definition(key, definition, content, search_text):
+        new_text = u''
+        if search_text is None:
+                #logger.info('Use default search text')
+                search_text = u'\\ntiglossaryentry{\\textbf{%s}}{definition}' % (key)
+                new_text = u'\\ntiglossaryentry{\\textbf{%s}}{%s}' % (key, definition)
+        elif search_text ==u'textbf':
+                search_text = u'\\textbf{%s}' %(key)
+                new_text = u'\\ntiglossaryentry{%s}{%s}' % (key, definition)
+        elif search_text == u'textit':
+                search_text = u'\\textit{%s}' %(key)
+                new_text = u'\\ntiglossaryentry{%s}{%s}' % (key, definition)
         new_content = content.replace(search_text, new_text)
         return new_content
 
---
+++ src/nti/contenttools/glossary_term_finder.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+This module is useful to find glossary terms (with particular pattern) inside a latex file 
+We need to provide a dictionary of glossary (stored in json file) to be able to replace text inside the latex file with ntiglossary{key}{definition}
+for example:
+	a latex files exported from docx format has glossary terms written using pattern '\\textbf{'...'}' where '...' refers to any terms
+	if we want to replace string like: '\\textbf{autorhythmicity}' with {\\ntiglossaryentry{autorhythmicity}{ability of cardiac muscle to initiate its own electrical impulse that triggers the mechanical contraction that pumps blood at a fixed pace without nervous or endocrine control}
+	it will work if we have 'autorhythmicity' in dictionary key and its value pair
+	we run this program by typing the following command:
+	./bin/nti_glossary_finder -s textbf -g 'json files' 'latex files'
+
+>> TODO: create glossary.json from txt file or docx file
+
+.. $Id: glossary_term_finder.py 52355 2014-10-28 17:04:00Z egawati.panjei $
+"""
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+DEFAULT_FORMAT_STRING = '[%(asctime)-15s] [%(name)s] %(levelname)s: %(message)s'
+
+import os
+import codecs
+import logging
+import argparse
+import threading
+import simplejson as json
+from zope.exceptions import log as ze_log
+
+from .glossary import glossary_check
+
+
+def _parse_args():
+	arg_parser = argparse.ArgumentParser( description="NTI Glossary Terms Finder" )
+	arg_parser.add_argument( 'filename', 
+							 help="The latex file" )
+	arg_parser.add_argument( '-g', '--glossary', 
+							 default=None, 
+							 help="json file contains glossary")
+	arg_parser.add_argument( '-s', '--search', 
+							 default=None, 
+							 help="string/text to replace")
+	return arg_parser.parse_args()
+
+def _title_escape( title ):
+	return title.replace(' ', '_').replace('-','_').replace(':','_')
+
+def _configure_logging(level='INFO'):
+	numeric_level = getattr(logging, level.upper(), None)
+	numeric_level = logging.INFO if not isinstance(numeric_level, int) else numeric_level
+	logging.basicConfig(level=numeric_level)
+	logging.root.handlers[0].setFormatter(ze_log.Formatter(DEFAULT_FORMAT_STRING))
+
+def _setup_configs():
+	_configure_logging()
+
+
+def main():
+	# Parse command line args
+	args = _parse_args()
+
+	_setup_configs()
+
+	filename = os.path.expanduser(args.filename)
+
+	# Verify the input file exists
+	if not os.path.exists(filename):
+		logger.info( 'The source file, %s, does not exist.',filename)
+		exit()
+
+	glossary_json = args.glossary
+	#Verify if the json file exists
+	if not os.path.exists(glossary_json):
+		logger.info('The json file, %s, does not exist.', filename)
+		exit()
+
+
+	json_str = glossary_check.get_file_content(glossary_json)
+	glossary_dict = json.loads(json_str)
+
+	search_text = args.search
+
+	#replace text in the latex file
+	if search_text is not None:
+		glossary_check.process_glossary(glossary_dict, filename, unicode(search_text))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
