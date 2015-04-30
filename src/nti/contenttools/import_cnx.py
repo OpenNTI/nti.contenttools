d0b3af1bbba3c42cdbe6ff9ed0d4aebb4fb24624
Egawati Panjei <egawati.panjei@nextthought.com>
2015-04-30 20:07:59 +0000

    add output directory as parameter to cnx_parser, add module import_cnx that will be used as an entry point
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@64208 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/contenttools/import_cnx.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id: import_cnx.py 58552 2015-01-29 23:10:30Z egawati.panjei $
+"""
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+import os
+import codecs
+import logging
+import argparse
+import simplejson as json
+
+from .util import string_replacer
+from .cnx.cnx_parser import CNXParser
+
+DEFAULT_FORMAT_STRING = '[%(asctime)-15s] [%(name)s] %(levelname)s: %(message)s'
+
+def _parse_args():
+	arg_parser = argparse.ArgumentParser( description="NTI CNX-Latex Parser" )
+	arg_parser.add_argument( 'inputfile', 
+							 help="The CNX directory where collection.xml is located" )
+	arg_parser.add_argument( '-o', '--output', 
+							 default='output',
+							 help="The output directory. The default is: %s" % 'output' )
+	return arg_parser.parse_args()
+
+def _title_escape( title ):
+	return string_replacer.rename_filename(title)
+
+def _configure_logging(level='INFO'):
+	numeric_level = getattr(logging, level.upper(), None)
+	numeric_level = logging.INFO if not isinstance(numeric_level, int) else numeric_level
+	logging.basicConfig(level=numeric_level)
+
+def _setup_configs():
+	_configure_logging()
+	
+def main():
+	# Parse command line args
+	args = _parse_args()
+
+	_setup_configs()
+
+	inputfile = os.path.expanduser(args.inputfile)
+	
+	# Verify the input file exists
+	if not os.path.exists( inputfile ):
+		logger.info( 'The source file, %s, does not exist.', inputfile )
+		exit()
+
+	# Create the output directory if it does not exist
+	if not os.path.exists( args.output ):
+		os.mkdir( args.output )
+
+	cnx = CNXParser(inputfile, args.output)
+	result =  cnx.process_collection()
+
+if __name__ == '__main__': # pragma: no cover
+	main()
