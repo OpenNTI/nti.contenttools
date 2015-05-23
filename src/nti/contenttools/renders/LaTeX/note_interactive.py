b6dfd8b48f6545cb9689b542d77e8b996cc5834b
Egawati Panjei <egawati.panjei@nextthought.com>
2015-05-23 00:04:56 +0000

    update note interactive renderer
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@65967 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttools/renders/LaTeX/note_interactive.py
+++ src/nti/contenttools/renders/LaTeX/note_interactive.py
@@ -13,7 +13,7 @@ import codecs
 from nti.contenttools import scoped_registry
 
 def note_interactive_rendered(self):
-	new_image_path = u'images/%s' %(self.image_path)
+	new_image_path = self.complete_image_path if self.complete_image_path is not None else u'images/%s' %(self.image_path)
 	# save image path infomation to file, in case content team wants to replace those images
 	try:
 		nticard_images_filename = scoped_registry.nticard_images_filename
@@ -26,7 +26,8 @@ def note_interactive_rendered(self):
 	with codecs.open(nticard_images_filename, 'a', 'utf-8') as fp:
 		fp.write("%s\n" %new_image_path)
 
-	caption = self.caption.render().rstrip()
+	caption = self.caption if isinstance(self.caption, unicode) or isinstance(self.caption, str) else self.caption.render().strip() 
+	notes = self.notes if isinstance(self.notes, unicode) or isinstance(self.notes, str) else self.notes.render().strip() 
 	result =  u'\n\\begin{nticard}{%s}\n\\label{%s}\n\\caption{%s}\n\\includegraphics{%s}\n%s\n\\end{nticard}\n' \
-	 		  % (self.link, self.label, caption, new_image_path, self.notes)
+	 		  % (self.link, self.label, caption, new_image_path, notes)
 	return result
