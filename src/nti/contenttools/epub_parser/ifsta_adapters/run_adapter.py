f58cb3ec7d5f4a440ddd08b0ea9b7b5ec05ffbc3
Egawati Panjei <egawati.panjei@nextthought.com>
2016-08-25 10:54:09 +0000

    epub - latex parser : ifsta : ignore some paragraph
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@95376 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttools/epub_parser/ifsta_adapters/run_adapter.py
+++ src/nti/contenttools/epub_parser/ifsta_adapters/run_adapter.py
@@ -96,31 +96,38 @@ class Paragraph( types.Paragraph ):
         me.reading_type = reading_type
         if 'id' in element.attrib : me.label = element.attrib['id']
         me.styles.extend(styles)
-        me = check_element_text(me, element)
-        me = check_child(me, element, reading_type)
-        me = check_element_tail(me, element)
         if u'class' in element.attrib:
-            paragraph_list = [u'Body-Text', u'Block-Text', 'ParaOverride']
-            section_list = [u'A-Head']
-            sidebar_list = [u'Case-History ParaOverride-1']
-            bullet_list = [u'Bullet ParaOverride-1']
-            if any(substring in element.attrib['class'] for substring in sidebar_list):
-                sidebar_class = Sidebar()
-                if u'Case-History' in element.attrib['class']:
-                    sidebar_class.title = u'Case History'
-                sidebar_class.children = me.children
-                me = sidebar_class
-            elif any(substring in element.attrib['class'] for substring in section_list):
-                me.styles.append('Section')
-            elif any(substring in element.attrib['class'] for substring in bullet_list):
-                bullet_class = UnorderedList()
-                new_item  = Item()
-                new_item.children = me.children
-                bullet_class.children = [new_item]
-                me = bullet_class
-            elif any(substring in element.attrib['class'] for substring in paragraph_list):
-                me.add_child(types.TextNode("\\\\\n"))
-                #from IPython.core.debugger import Tracer; Tracer()()
+            if element.attrib['class'] == u"ParaOverride-1":
+                pass
+            else:
+                me = check_element_text(me, element)
+                me = check_child(me, element, reading_type)
+                me = check_element_tail(me, element)
+                paragraph_list = [u'Body-Text', u'Block-Text', 'ParaOverride']
+                section_list = [u'A-Head']
+                sidebar_list = [u'Case-History ParaOverride-1']
+                bullet_list = [u'Bullet ParaOverride-1']
+                if any(substring in element.attrib['class'] for substring in sidebar_list):
+                    sidebar_class = Sidebar()
+                    if u'Case-History' in element.attrib['class']:
+                        sidebar_class.title = u'Case History'
+                    sidebar_class.children = me.children
+                    me = sidebar_class
+                elif any(substring in element.attrib['class'] for substring in section_list):
+                    me.styles.append('Section')
+                elif any(substring in element.attrib['class'] for substring in bullet_list):
+                    bullet_class = UnorderedList()
+                    new_item  = Item()
+                    new_item.children = me.children
+                    bullet_class.children = [new_item]
+                    me = bullet_class
+                elif any(substring in element.attrib['class'] for substring in paragraph_list):
+                    me.add_child(types.TextNode("\\\\\n"))
+                    #from IPython.core.debugger import Tracer; Tracer()()
+        else:
+            me = check_element_text(me, element)
+            me = check_child(me, element, reading_type)
+            me = check_element_tail(me, element)
         return me
 
 class Hyperlink( types.Hyperlink ):
