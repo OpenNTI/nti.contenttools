4e56ae597bc40f9aac6aae9caad8e5007b09e9c0
Egawati Panjei <egawati.panjei@nextthought.com>
2016-09-07 09:24:25 +0000

    add mathcount parser
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@96367 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/contenttools/mathcounts_parser/adapters/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id: __init__.py 81889 2016-01-28 16:40:10Z carlos.sanchez $
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from nti.contenttools.html_parser.adapters.run_adapter import adapt
---
+++ src/nti/contenttools/mathcounts_parser/adapters/image_adapter.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id: image_adapter.py 81888 2016-01-28 16:35:07Z carlos.sanchez $
+"""
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+import os
+import shutil
+import requests
+from lxml.html import HtmlComment
+
+from PIL import Image as PILImage
+import os
+try:
+    import cStringIO as StringIO
+except:
+    import StringIO
+
+from ... import types
+from ... import scoped_registry
+
+class Image(types.Image):
+	@classmethod
+	def process(cls, element, inline_image=False):
+		me = cls()
+		path = element.attrib['src']
+		if u'../' in path :
+			path = path.replace('../', '')
+			image_url = u'%s/%s' %(scoped_registry.image_url, path)
+			filepath = u'%s/%s' %(scoped_registry.output_directory, path)
+			save_image(image_url, filepath)
+
+		head, filename = os.path.split(path)
+		me.predefined_image_path = True
+		me.path = path
+		me.inline_image = inline_image
+		if 'alt' in element.attrib.keys():
+		    me.caption = types.TextNode(element.attrib['alt'])
+		return me	
+
+def save_image(image_url, filepath):
+	r = requests.get(image_url, stream=True)
+	if r.status_code == 200:
+		if not os.path.exists(os.path.dirname(filepath)):
+			os.makedirs(os.path.dirname(filepath))
+		with open(filepath, 'wb') as f:
+		    r.raw.decode_content = True
+		    shutil.copyfileobj(r.raw, f)   
+
+
+	
