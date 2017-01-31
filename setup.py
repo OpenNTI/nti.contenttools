7394e1ef322fa43c4e6e2e18872259e5bd4e3405
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-01-31 15:51:30 +0000

    remove dep
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@105531 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -4,48 +4,45 @@ from setuptools import setup, find_packages
 VERSION = '0.0.0'
 
 entry_points = {
-	"z3c.autoinclude.plugin": [
-		'target = nti.dataserver',
-	],
-	'console_scripts': [
-		'nti_import_docx = nti.contenttools.word2latex:main',
-		'nti_import_epub = nti.contenttools.import_epub:main',
-		'nti_glossary_finder = nti.contenttools.glossary_term_finder:main',
+    "z3c.autoinclude.plugin": [
+        'target = nti.contenttools',
+    ],
+    'console_scripts': [
+        'nti_import_docx = nti.contenttools.word2latex:main',
+        'nti_import_epub = nti.contenttools.import_epub:main',
+        'nti_glossary_finder = nti.contenttools.glossary_term_finder:main',
         'nti_glossary_exporter = nti.contenttools.glossary_json_exporter:main',
         'nti_import_cnx = nti.contenttools.import_cnx:main',
         'nti_epub_latex_parser = nti.contenttools.parse_epub_latex:main',
         'nti_html_latex_parser = nti.contenttools.parse_html_latex:main',
-        'nti_mathcounts_html_latex_parser = nti.contenttools.parse_mathcounts_html_latex:main' 
-	]
+        'nti_mathcounts_html_latex_parser = nti.contenttools.parse_mathcounts_html_latex:main'
+    ]
 }
 
 setup(
-	name='nti.contenttools',
-	version=VERSION,
-	author='Jason Madden',
-	author_email='jason@nextthought.com',
-	description="NTI Content Tools",
-	long_description=codecs.open('README.rst', encoding='utf-8').read(),
-	license='Proprietary',
-	keywords='Content tools',
-	classifiers=[
+    name='nti.contenttools',
+    version=VERSION,
+    author='Jason Madden',
+    author_email='jason@nextthought.com',
+    description="NTI Content Tools",
+    long_description=codecs.open('README.rst', encoding='utf-8').read(),
+    license='Proprietary',
+    keywords='Content tools',
+    classifiers=[
 		'Intended Audience :: Developers',
 		'Natural Language :: English',
 		'Operating System :: OS Independent',
 		'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
+		'Programming Language :: Python :: 2.7',
     ],
-	packages=find_packages('src'),
-	package_dir={'': 'src'},
-	namespace_packages=['nti'],
-	install_requires=[
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['nti'],
+    install_requires=[
         'setuptools',
         'gevent',
         'lxml',
         'Pillow',
-        'pyramid',
         'simplejson',
         'zope.component',
         'zope.exceptions',
