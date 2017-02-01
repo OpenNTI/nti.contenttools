7283dc82cd523d93c7af4d1300ca8f3ff0593c25
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-02-01 02:54:15 +0000

    start cleaning up
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@105585 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttools/interfaces.py
+++ src/nti/contenttools/interfaces.py
@@ -8,3 +8,58 @@ from __future__ import print_function, unicode_literals, absolute_import, divisi
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
+
+from zope import interface
+
+from nti.schema.field import Object
+from nti.schema.field import UniqueIterable
+
+class IRenderContext(interface.Interface):
+    """
+    Renderer context
+    """
+
+class _INode(interface.Interface):
+    """
+    Basic interface for nodes
+    """
+
+class INode(_INode):
+
+    children = UniqueIterable(Object(_INode, title='the node'),
+                              title='List of nodes')
+
+    def add(node):
+        """
+        add a ndoe
+        """
+
+    def remove(node):
+        """
+        remove a node
+        """
+        
+    def render(context):
+        """
+        Render this not using the specified context
+        
+        :param context: A :class:`IRenderContext` object 
+        """
+
+class IRunNode(INode):
+    """
+    A run node
+    """
+
+class IRenderer(interface.Interface):
+    """
+    Marker interface for node renderers
+    """
+
+    def render(context, node, *args, **kwargs):
+        """
+        Render this not using the specified context
+        
+        :param context: A :class:`IRenderContext` object 
+        :param context: A :class:`INode` object to render
+        """
---
+++ src/nti/contenttools/renderers/LaTeX/run.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+
+from zope import component
+from zope import interface
+
+from nti.contenttools.interfaces import IRunNode
+from nti.contenttools.interfaces import IRenderer
+
+
+@component.adapter(IRunNode)
+@interface.implementer(IRenderer)
+class RunRenderer(object):
+
+    __slots__ = ('node',)
+
+    def __init__(self, node):
+        self.node = node
+
+    def render(self, context, node=None, *args, **kwargs):
+        node = self.node if node is None else node
+    __call__ = render
