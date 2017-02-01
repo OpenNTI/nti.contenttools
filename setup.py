38a52817578748b85ccf42dcee53b1483e75f080
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-02-01 06:14:36 +0000

    add nti.schema
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@105609 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -51,7 +51,8 @@ setup(
         'zope.location',
         'zope.security',
         'zope.traversing',
-        'nti.contentfragments'
+        'nti.contentfragments',
+        'nti.schema'
     ],
     entry_points=entry_points
 )
