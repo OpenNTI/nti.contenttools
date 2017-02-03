1a7e10975f992011ec20f1bdbc5189f0af796c7b
Egawati Panjei <egawati.panjei@nextthought.com>
2017-02-03 04:50:14 +0000

    meanwhile fix the import
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@105742 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttools/renders/LaTeX/mathcounts.py
+++ src/nti/contenttools/renders/LaTeX/mathcounts.py
@@ -13,46 +13,47 @@ import re
 from ... import types
 from .base import base_renderer, list_renderer
 
+
 def naq_symmath_part_renderer(self):
-	"""
-	render NaqSymmathPart
-	"""
-	content = self.text.render()
-	if content.isspace() or len(content) == 0:
-		logger.warn('Empty NaqSymmathPart')
-		return u''
-	
-	solution_part = self.solution.render()
+    """
+    render NaqSymmathPart
+    """
+    content = self.text.render()
+    if content.isspace() or len(content) == 0:
+        logger.warn('Empty NaqSymmathPart')
+        return u''
 
-	#result = u"\n\\begin{naqsymmathpart}\n%s\n%s\n\\end{naqsymmathpart}\n\n" %(content, solution_part)
+    solution_part = self.solution.render()
 
-	result = u"\n\\begin{naquestion}[individual=true]\n\\label{%s}\n\\begin{naqsymmathpart}\n%s\n%s\n\\end{naqsymmathpart}\n\\end{naquestion}\n\n" %(self.label, content, solution_part)
+    #result = u"\n\\begin{naqsymmathpart}\n%s\n%s\n\\end{naqsymmathpart}\n\n" %(content, solution_part)
 
-	return result
+    result = u"\n\\begin{naquestion}[individual=true]\n\\label{%s}\n\\begin{naqsymmathpart}\n%s\n%s\n\\end{naqsymmathpart}\n\\end{naquestion}\n\n" % (
+        self.label, content, solution_part)
+
+    return result
 
 
 def naq_symmath_part_solution_renderer(self):
-	"""
-	render NaqSymmathPartSolution
-	"""
-	solution = base_renderer(self)
+    """
+    render NaqSymmathPartSolution
+    """
+    solution = base_renderer(self)
 
-	result = u"\\begin{naqsolutions}\n%s\\end{naqsolutions}" %(solution)
+    result = u"\\begin{naqsolutions}\n%s\\end{naqsolutions}" % (solution)
 
-	return result
+    return result
 
 
 def naq_symmath_part_solution_value_renderer(self):
-	"""
-	render NaqSymmathPartSolution
-	"""
-	if self.option.isspace or len (self.option) == 0:
-		option = self.option
-	else:
-		option = u"<%s>" %(self.option)
-	
-	value = self.value
+    """
+    render NaqSymmathPartSolution
+    """
+    if self.option.isspace or len(self.option) == 0:
+        option = self.option
+    else:
+        option = u"<%s>" % (self.option)
 
-	result = u"\\naqsolution[1] %s %s\n" %(option, value)
-	return result
+    value = self.value
 
+    result = u"\\naqsolution[1] %s %s\n" % (option, value)
+    return result
