689ffacf36827a67d5821591c98f54b3e0b76208
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-02-09 03:57:34 +0000

    add two helper funcs
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@106152 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttools/renderers/LaTeX/base.py
+++ src/nti/contenttools/renderers/LaTeX/base.py
@@ -34,6 +34,20 @@ def render_children(context, node):
 base_renderer = render_children  # alias
 
 
+def render_command(context, command, node, optional=u''):
+    optional = optional or u''
+    optional = u'[%s]' % optional if optional else optional
+    context.write(u'\\%s%s{' % (command, optional))
+    render_children(context, node)
+    context.write('}')
+    return node
+
+def render_environment(context, element, node, optional=''):
+    context.write(u'\\begin{%s}%s\n' % (element, optional or u''))
+    render_children(context, node)
+    context.write(u'\n\\end{%s}\n' % element)
+    return node
+
 def render_output(node):
     result = DefaultRendererContext(name="LaTeX")
     render_node(result, node)
