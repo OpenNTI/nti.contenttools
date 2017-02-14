a2f0c6048bba29ab22eed690b81496dee4d72f4a
Egawati Panjei <egawati.panjei@nextthought.com>
2017-02-14 14:35:16 +0000

    docutils latex ext : update test
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@106501 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttools/docutils_extension/writer/tests/data/references.rst
+++ src/nti/contenttools/docutils_extension/writer/tests/data/references.rst
@@ -2,6 +2,13 @@
 References
 ++++++++++
 
+++++++++++
+References
+++++++++++
+
 This paragraph will have different styles of links that RST supports. Such as `External Hyperlink Targets`_. Also the embedded URI format, details found `here <http://docutils.sourceforge.net/docs/ref/rst/restructuredtext.html#embedded-uris>`_.
 
-test fails
+We will also need to test internal hyperlink targets. Like linking to the `Inline Styles`_ section. I wonder if the embedded URI format will for for this `Test Link <Emphasis>`_ (It Treats it as a URI so I guess it doesn't).
+
+.. _External Hyperlink Targets: http://docutils.sourceforge.net/docs/user/rst/quickref.html#hyperlink-targets
+
--- src/nti/contenttools/docutils_extension/writer/tests/test_writer_extension.py
+++ src/nti/contenttools/docutils_extension/writer/tests/test_writer_extension.py
@@ -5,7 +5,6 @@
 """
 
 from __future__ import print_function, unicode_literals, absolute_import, division
-from hamcrest.library.collection.issequence_containinginorder import contains
 __docformat__ = "restructuredtext en"
 
 from hamcrest import assert_that
@@ -40,7 +39,6 @@ class TestNTIWriter(ContentToolsTestCase):
     def test_package_requirements(self):
         rst = read_file(get_relative_path('data/section.rst'))
         tex = latex.generate_tex_from_rst(rst)
-        #assert_that(tex, contains_string(u'\\usepackage{hyperref}'))
         assert_that(tex, contains_string(u'\\usepackage{ulem}'))
         assert_that(tex, contains_string(u'\\usepackage{Tabbing}'))
         assert_that(tex, contains_string(u'\\usepackage{textgreek}'))
@@ -58,37 +56,39 @@ class TestWriterExtension(ContentToolsTestCase):
         self.assertTrue(isinstance(rst, basestring))
         tex = latex.generate_tex_from_rst(rst)
         self.assertTrue(isinstance(tex, basestring))
-        assert_that(tex,contains_string(u'\\section'))
-        assert_that(tex,contains_string(u'\\subsection'))
-    
+        assert_that(tex, contains_string(u'\\section'))
+        assert_that(tex, contains_string(u'\\subsection'))
+
     def test_list(self):
         rst = read_file(get_relative_path('data/lists.rst'))
         self.assertTrue(isinstance(rst, basestring))
         tex = latex.generate_tex_from_rst(rst)
         self.assertTrue(isinstance(tex, basestring))
-        assert_that(tex,contains_string(u'\\begin{itemize}'))
-        assert_that(tex,contains_string(u'\\end{itemize}'))
-        assert_that(tex,contains_string(u'\\begin{enumerate}'))
-        assert_that(tex,contains_string(u'\\end{enumerate}'))
-    
+        assert_that(tex, contains_string(u'\\begin{itemize}'))
+        assert_that(tex, contains_string(u'\\end{itemize}'))
+        assert_that(tex, contains_string(u'\\begin{enumerate}'))
+        assert_that(tex, contains_string(u'\\end{enumerate}'))
+
     def test_nested_list(self):
         rst = read_file(get_relative_path('data/bulleted_list.rst'))
         self.assertTrue(isinstance(rst, basestring))
         tex = latex.generate_tex_from_rst(rst)
         self.assertTrue(isinstance(tex, basestring))
-        assert_that(tex,contains_string(u'\\begin{itemize}'))
-        assert_that(tex,contains_string(u'\\end{itemize}'))
-        print(tex)
-    
+        assert_that(tex, contains_string(u'\\begin{itemize}'))
+        assert_that(tex, contains_string(u'\\end{itemize}'))
+
     def test_references(self):
         rst = read_file(get_relative_path('data/references.rst'))
         self.assertTrue(isinstance(rst, basestring))
         tex = latex.generate_tex_from_rst(rst)
         self.assertTrue(isinstance(tex, basestring))
-        #todo : need to fix the external hyperlink targets 
+        assert_that(tex, contains_string(u'{hyperref}'))
         assert_that(tex, contains_string(u'{External Hyperlink Targets}'))
-        
-        
+        assert_that(tex, contains_string(
+            u'\\href{http://docutils.sourceforge.net/docs/ref/rst/restructuredtext.html\#embedded-uris}{here}'))
+        # todo : check inline styles
+        assert_that(tex, contains_string(u'\\hyperref[inline-styles]{Inline Styles}'))
+
     def test_custom(self):
         rst = read_file(get_relative_path('data/custom.rst'))
         self.assertTrue(isinstance(rst, basestring))
@@ -96,6 +96,8 @@ class TestWriterExtension(ContentToolsTestCase):
         self.assertTrue(isinstance(tex, basestring))
         write_file(get_relative_path('test_result/custom.tex'), tex)
         assert_that(tex, contains_string(u'\\textbf{\\emph{bolditalic}}'))
-        assert_that(tex,contains_string(u'\\textbf{\\underline{boldunderline}}'))
-        assert_that(tex,contains_string(u'\\emph{\\underline{italicunderlined}}'))
-        assert_that(tex,contains_string(u'\\textbf{\\emph{\\underline{bolditalicunderlined}}}'))
+        assert_that(tex, contains_string(u'\\textbf{\\underline{boldunderline}}'))
+        assert_that(tex, contains_string(u'\\emph{\\underline{italicunderlined}}'))
+        assert_that(
+            tex,
+            contains_string(u'\\textbf{\\emph{\\underline{bolditalicunderlined}}}'))
