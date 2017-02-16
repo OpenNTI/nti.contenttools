afcdd143dd3a868054c4c95b88d80b1df34180c8
Egawati Panjei <egawati.panjei@nextthought.com>
2017-02-16 03:54:26 +0000

    add media module and test
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@106712 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/contenttools/types/media.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id: node.py 106584 2017-02-15 04:19:57Z carlos.sanchez $
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from zope import interface
+
+from nti.contenttools.types.interfaces import IDocxImage
+from nti.contenttools.types.interfaces import IImage
+from nti.contenttools.types.interfaces import IVideo
+
+
+from nti.contenttools.types.node import DocumentStructureNode
+
+from nti.schema.fieldproperty import createFieldProperties
+
+@interface.implementer(IImage)
+class Image(DocumentStructureNode):
+    createFieldProperties(IImage)
+
+@interface.implementer(IDocxImage)
+class DocxImage(Image):
+    createFieldProperties(IDocxImage)
+
+@interface.implementer(IVideo)
+class Video(DocumentStructureNode):
+    createFieldProperties(IVideo)
---
+++ src/nti/contenttools/types/tests/test_media.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
+
+from hamcrest import is_
+from hamcrest import is_not
+from hamcrest import assert_that
+from hamcrest import has_property
+does_not = is_not
+
+from nti.testing.matchers import validly_provides
+from nti.testing.matchers import verifiably_provides
+
+from nti.contenttools.types.interfaces import IDocxImage
+from nti.contenttools.types.interfaces import IImage
+from nti.contenttools.types.interfaces import IVideo
+
+from nti.contenttools.types.media import Image
+from nti.contenttools.types.media import DocxImage
+from nti.contenttools.types.media import Video
+
+from nti.contenttools.tests import ContentToolsTestCase
+
+class TestMedia(ContentToolsTestCase):
+
+    def test_image(self):
+        node = Image()
+        assert_that(node, validly_provides(IImage))
+        assert_that(node, verifiably_provides(IImage))
+        assert_that(node, has_property('path', is_(u'')))
+        assert_that(node, has_property('caption', is_(u'')))
+        assert_that(node, has_property('width', is_(0)))
+        assert_that(node, has_property('height', is_(0)))
+        assert_that(node, has_property('equation_image', is_(False)))
+        assert_that(node, has_property('inline_image', is_(False)))
+        assert_that(node, has_property('predefined_image_path', is_(False)))
+    
+    def test_docx_image(self):
+        node = DocxImage()
+        assert_that(node, validly_provides(IDocxImage))
+        assert_that(node, verifiably_provides(IDocxImage))
+        assert_that(node, has_property('path', is_(u'')))
+        assert_that(node, has_property('caption', is_(u'')))
+        assert_that(node, has_property('width', is_(0)))
+        assert_that(node, has_property('height', is_(0)))
+        assert_that(node, has_property('equation_image', is_(False)))
+        assert_that(node, has_property('inline_image', is_(False)))
+        assert_that(node, has_property('predefined_image_path', is_(False)))
+    
+    def test_video(self):
+        node = Video()
+        assert_that(node, validly_provides(IVideo))
+        assert_that(node, verifiably_provides(IVideo))
+        assert_that(node, has_property('path', is_(u'')))
+        assert_that(node, has_property('caption', is_(u'')))
+        assert_that(node, has_property('thumbnail', is_(u'')))
+        assert_that(node, has_property('width', is_(0)))
+        assert_that(node, has_property('height', is_(0)))
+        
+        
