f959fa8ba530adaf0654c296ceb2415f6541acdb
Egawati Panjei <egawati.panjei@nextthought.com>
2017-02-17 13:28:25 +0000

    cleanup
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@106861 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttools/renderers/LaTeX/table.py
+++ src/nti/contenttools/renderers/LaTeX/table.py
@@ -25,14 +25,16 @@ from nti.contenttools.types.interfaces import ITable
 from nti.contenttools.types.table import TBody
 from nti.contenttools.types.table import THead
 
+
 def set_number_of_table_col(node):
     header_index = find_table_child(THead, node)
     if header_index is not None:
         node.set_number_of_col_header(node.children[header_index].number_of_col)
-    
+
     body_index = find_table_child(TBody, node)
     if body_index is not None:
         node.set_number_of_col_body(node.children[body_index].number_of_col)
+
 
 def find_table_child(type_, me):
     list_ = me.children
@@ -41,17 +43,22 @@ def find_table_child(type_, me):
             return index
     return None
 
-def get_string_col(number_of_col,border):
+
+def get_string_col(number_of_col, border):
     cols = []
     cols_append = cols.append
 
-    if border and number_of_col > 0 : cols_append(u'|')
+    if border and number_of_col > 0:
+        cols_append(u'|')
 
-    for i in range (number_of_col):
-        if border: cols_append(u'l|')
-        else: cols_append(u' l ')
+    for i in range(number_of_col):
+        if border:
+            cols_append(u'l|')
+        else:
+            cols_append(u' l ')
 
     return u''.join(cols)
+
 
 def process_table_html(context, node, string_col):
     body = render_children_output(node)
@@ -67,17 +74,20 @@ def process_table_html(context, node, string_col):
         return result % (label, string_col, body,)
     elif node.label is None and node.caption is not None:
         caption = node.caption.render()
-        #TODO: the text_label only works for openstax epub, we need to modify the following line
-        text_label = node.caption.children[0].render() + node.caption.children[1].children[0].render()
+        # TODO: the text_label only works for openstax epub, we need to modify the
+        # following line
+        text_label = node.caption.children[
+            0].render() + node.caption.children[1].children[0].render()
         caption = caption.replace(text_label, u'')
         result = u'\n\\begin{table}\n\\caption {%s}\n\\begin{tabular}{%s}\n%s\\end{tabular}\n\\end{table}\n'
         return result % (caption, string_col, body)
     elif node.label is None and node.caption is None and node.type_ is None:
         result = u'\n\\begin{table}\n\\begin{tabular}{%s}\n%s\\end{tabular}\n\\end{table}\n'
         return result % (string_col, body)
-    elif node.type_== u'simplelist':
+    elif node.type_ == u'simplelist':
         result = u'\n%s\n\\newline '
-        return result %(body)
+        return result % (body)
+
 
 def render_html_table(context, node):
     set_number_of_table_col(node)
@@ -94,54 +104,67 @@ def render_html_table(context, node):
     result = process_table_html(context, node, string_col)
     context.write(result)
 
+
 def render_html_table_row(context, node):
     result = []
     for child in node.children:
         result.append(child.render())
     if node.border:
-        output =  u' & '.join(result) + u'\\\\ \hline\n' 
+        output = u' & '.join(result) + u'\\\\ \hline\n'
     elif node.type_ == u'simplelist':
-        output =  u''.join(result) + u'\\\\\n'
-    else :
+        output = u''.join(result) + u'\\\\\n'
+    else:
         output = u' & '.join(result) + u'\\\\\n'
     context.write(output)
 
+
 def render_html_table_cell(context, node):
     result = render_children_output(node).rstrip()
-    if result.find(u'&') > 0 and result.find(u'\\&') < 0 : result = result.replace(u'&', u'\\&')
+    if result.find(u'&') > 0 and result.find(u'\\&') < 0:
+        result = result.replace(u'&', u'\\&')
     if result.isspace() or result is None or result == u'':
         result = u' ~ '
     elif node.colspan > 1:
-        result = get_multicolumn(node.colspan, node.border, node.is_first_cell_in_the_row, result)
+        result = get_multicolumn(
+            node.colspan,
+            node.border,
+            node.is_first_cell_in_the_row,
+            result)
     context.write(result)
 
 
 def get_multicolumn(col_span, border, first_cell, cell_string):
     if border:
-        if first_cell: result = u'\\multicolumn{%s}{|l|}{%s} ' %(col_span, cell_string)
-        else: result = u'\\multicolumn{%s}{l|}{%s} ' %(col_span, cell_string)
-    else: result = u'\\multicolumn{%s}{l}{%s} ' %(col_span, cell_string)        
+        if first_cell:
+            result = u'\\multicolumn{%s}{|l|}{%s} ' % (col_span, cell_string)
+        else:
+            result = u'\\multicolumn{%s}{l|}{%s} ' % (col_span, cell_string)
+    else:
+        result = u'\\multicolumn{%s}{l}{%s} ' % (col_span, cell_string)
     return result
+
 
 def render_html_tbody(context, node):
     result = render_children_output(node)
     if node.border is not None:
-        result =  u'\\hline\n%s' %(result)
+        result = u'\\hline\n%s' % (result)
     context.write(result)
+
 
 def render_html_theader(context, node):
     parent = node.__parent__
     if parent:
         result = u'\\hline %s \\hline\n' if parent.border else u'%s'
         base = render_children_output(node)
-        context.write(result %(base))
+        context.write(result % (base))
+
 
 def render_html_tfooter(context, node):
     parent = node.__parent__
     if parent:
         result = u'\\hline %s \\hline\n' if parent.border else u'%s'
         base = render_children_output(node)
-        context.write(result %(base))
+        context.write(result % (base))
 
 
 @interface.implementer(IRenderer)
@@ -162,21 +185,26 @@ class RendererMixin(object):
 class HTMLTableRenderer(RendererMixin):
     func = staticmethod(render_html_table)
 
+
 @component.adapter(IRow)
 class HTMLTableRowRenderer(RendererMixin):
     func = staticmethod(render_html_table_row)
+
 
 @component.adapter(ICell)
 class HTMLTableCellRenderer(RendererMixin):
     func = staticmethod(render_html_table_cell)
 
+
 @component.adapter(ITHead)
 class HMTLTHeaderRenderer(RendererMixin):
     func = staticmethod(render_html_theader)
 
+
 @component.adapter(ITFoot)
 class HMTLTFooterRenderer(RendererMixin):
     func = staticmethod(render_html_tfooter)
+
 
 @component.adapter(ITBody)
 class HMTLTBodyRenderer(RendererMixin):
