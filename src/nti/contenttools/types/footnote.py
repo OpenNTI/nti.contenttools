7f3fff0e8602ee1f2d13bf09f61c7277e7ba2abb
Egawati Panjei <egawati.panjei@nextthought.com>
2017-02-17 03:02:49 +0000

    add footnote module
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@106833 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/contenttools/types/footnote.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id: footnote.py 106584 2017-02-15 04:19:57Z carlos.sanchez $
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from zope import interface
+
+from nti.contenttools.types.interfaces import IFootnote
+from nti.contenttools.types.interfaces import IFootnoteText
+from nti.contenttools.types.interfaces import IFootnoteMark
+
+from nti.contenttools.types.node import DocumentStructureNode
+
+from nti.schema.fieldproperty import createFieldProperties
+
+@interface.implementer(IFootnote)
+class Footnote(DocumentStructureNode):
+    createFieldProperties(IFootnote)
+
+@interface.implementer(IFootnoteText)
+class FootnoteText(DocumentStructureNode):
+    createFieldProperties(IFootnoteText)
+
+@interface.implementer(IFootnoteMark)
+class FootnoteMark(DocumentStructureNode):
+    createFieldProperties(IFootnoteMark)
---
+++ src/nti/contenttools/types/tests/test_footnote.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
+
+from hamcrest import is_
+from hamcrest import is_not
+from hamcrest import assert_that
+from hamcrest import has_property
+does_not = is_not
+
+from nti.testing.matchers import validly_provides
+from nti.testing.matchers import verifiably_provides
+
+from nti.contenttools.types.interfaces import IFootnote
+from nti.contenttools.types.interfaces import IFootnoteText
+from nti.contenttools.types.interfaces import IFootnoteMark
+
+from nti.contenttools.types.footnote import Footnote
+from nti.contenttools.types.footnote import FootnoteText
+from nti.contenttools.types.footnote import FootnoteMark
+
+from nti.contenttools.tests import ContentToolsTestCase
+
+class TestFootnote(ContentToolsTestCase):
+    def test_footnote(self):
+        node = Footnote()
+        assert_that(node, validly_provides(IFootnote))
+        assert_that(node, verifiably_provides(IFootnote))
+        assert_that(node, has_property('text', is_(None)))
+        assert_that(node, has_property('label', is_(None)))
+        
+    def test_footnote_text(self):
+        node = FootnoteText()
+        assert_that(node, validly_provides(IFootnoteText))
+        assert_that(node, verifiably_provides(IFootnoteText))
+        assert_that(node, has_property('text', is_(None)))
+        assert_that(node, has_property('label', is_(None)))
+        assert_that(node, has_property('num', is_(None)))
+    
+    def test_footnote_mark(self):
+        node = FootnoteMark()
+        assert_that(node, validly_provides(IFootnoteMark))
+        assert_that(node, verifiably_provides(IFootnoteMark))
+        assert_that(node, has_property('text', is_(None)))
+        assert_that(node, has_property('num', is_(None)))
