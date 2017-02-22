1cee3e0af9fd814f09215f1b46b0e7a923a8d8f2
Egawati Panjei <egawati.panjei@nextthought.com>
2017-02-22 11:36:33 +0000

    start rewrite math ml element renderers
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@107148 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/contenttools/renderers/LaTeX/math.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id: math.py 107145 2017-02-22 10:50:18Z egawati.panjei $
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from zope import component
+from zope import interface
+
+from nti.contenttools.renderers.LaTeX.base import render_children
+from nti.contenttools.renderers.LaTeX.base import render_environment
+from nti.contenttools.renderers.LaTeX.base import render_children_output
+
+from nti.contenttools.types.interfaces import IMtd
+from nti.contenttools.types.interfaces import IMtr
+from nti.contenttools.types.interfaces import IMath
+from nti.contenttools.types.interfaces import IMRow
+from nti.contenttools.types.interfaces import IMSup
+from nti.contenttools.types.interfaces import IMSub
+from nti.contenttools.types.interfaces import IMFrac
+from nti.contenttools.types.interfaces import IMOver
+from nti.contenttools.types.interfaces import IMRoot
+from nti.contenttools.types.interfaces import IMText
+from nti.contenttools.types.interfaces import IMsqrt
+from nti.contenttools.types.interfaces import IMSpace
+from nti.contenttools.types.interfaces import IMTable
+from nti.contenttools.types.interfaces import IMUnder
+from nti.contenttools.types.interfaces import IMathRun
+from nti.contenttools.types.interfaces import IMFenced
+from nti.contenttools.types.interfaces import IMSubSup
+from nti.contenttools.types.interfaces import IMMenclose
+from nti.contenttools.types.interfaces import IMUnderover
+from nti.contenttools.types.interfaces import IMMprescripts
+from nti.contenttools.types.interfaces import IMMultiscripts
+
+
+"""
+rendering MathML element
+"""
+
+def render_math_html(context, node):
+    """
+    render element <math>
+    """
+    if len(node.children) == 0:
+        context.write(u'')
+    elif node.equation_type == u'inline' : 
+        context.write(u'\\(')
+        render_children(context, node)
+        context.write(u'\\)')
+    else:
+        context.write(u'\\[')
+        render_children(context, node)
+        context.write(u'\\]')
+    return node
+
+def render_mrow(context,node):
+    return render_children(context, node)
+
+def render_mfenced(context, node):
+    """
+    render element <mfenced>
+    """
+    if len(node.children) > 0:
+        if IMTable.providedBy(node.children[0]):
+            return set_matrix_border(context, node)
+        elif IMRow.providedBy(node.children[0]):
+            if node.children[0].children:
+                if IMTable.providedBy(node.children[0].children[0]):
+                    return set_matrix_border(context, node)
+                else:
+                    return set_mfenced_without_border(context, node)
+            else:
+                return set_mfenced_without_border(context, node) 
+        else:
+            return set_mfenced_without_border(context, node)
+    else : 
+        context.write(u'')
+    return node
+
+
+def set_matrix_border(context, node):
+    if node.opener == u'[':
+        context.writer(u'\\begin{bmatrix}\n')
+        render_children(context, node)
+        context.writer(u'\\end{bmatrix}\n')
+    elif node.opener == u'(':
+        context.writer(u'\\begin{pmatrix}\n')
+        render_children(context, node)
+        context.writer(u'\\end{pmatrix}\n')
+    else:
+        context.writer(u'\\begin{matrix}\n')
+        render_children(context, node)
+        context.writer(u'\\end{matrix}\n')
+    return node
+
+def set_mfenced_without_border(context, node):
+    context.write(node.opener)
+    render_children(context, node)
+    context.write(node.close)
+    return node
+    
+
+def render_mrun(context, node):
+    """
+    render MathRun node
+    """
+    return render_children(context, node)
+
