aad335eb90b3073f7c6c22088ea88c1d2015d226
Egawati Panjei <egawati.panjei@nextthought.com>
2017-03-01 07:52:32 +0000

    implement run node tex renderer
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@107705 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttools/renderers/LaTeX/run.py
+++ src/nti/contenttools/renderers/LaTeX/run.py
@@ -12,11 +12,66 @@ logger = __import__('logging').getLogger(__name__)
 from zope import component
 from zope import interface
 
+from nti.contenttools.renderers.LaTeX.base import render_children
+from nti.contenttools.renderers.LaTeX.base import render_children_output
+
 from nti.contenttools.renderers.interfaces import IRenderer
 
 from nti.contenttools.types.interfaces import IRunNode
 
-# TODO: Implement render
+
+def render_run_command(command, base):
+    return u'\\%s{%s}' %(command, base)
+
+def _textbf(base):
+    return render_run_command('textbf', base)
+
+def _modified(base):
+    return render_run_command('modified', base)
+
+def _textit(base):
+    return render_run_command('textit', base)
+ 
+def _strikeout(base):
+    return render_run_command('strikeout', base)
+
+def _subscript(base):
+    return render_run_command('textsubscript', base)
+
+def _uline(base):
+    return render_run_command('uline', base)
+
+def _superscript(base):
+    return render_run_command('textsuperscript', base)
+
+def render_run_node(context, node):
+    STYLES = { 'bold': _textbf,
+               'inserted': _modified,
+               'italic': _textit,
+               'strike': _strikeout,
+               'sub': _subscript,
+               'underline': _uline,
+               'sup': _superscript,
+               'subscript': _subscript,
+               'superscript': _superscript}
+    
+    IGNORED_STYLE = [u'apple-converted-space', u'HTMLDefinition', u'cnxn-target']
+    
+    if node.styles:
+        base = render_children_output(node)
+        for style in node.styles:
+            if style in STYLES.keys():
+                base = STYLES[style](base)
+            elif style in IGNORED_STYLE :
+                pass
+            else:
+                logger.info('Unhandled run style: %s' % style)
+        context.write(base)
+    else:
+        render_children(context, node)
+    
+    return node
+
 @component.adapter(IRunNode)
 @interface.implementer(IRenderer)
 class RunRenderer(object):
@@ -28,4 +83,5 @@ class RunRenderer(object):
 
     def render(self, context, node=None, *args, **kwargs):
         node = self.node if node is None else node
+        return render_run_node(context, node)
     __call__ = render
