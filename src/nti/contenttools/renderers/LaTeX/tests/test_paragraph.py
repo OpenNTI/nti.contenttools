8f5752c9e5849b55e07bc6b233f6c6a17430bb5a
Egawati Panjei <egawati.panjei@nextthought.com>
2017-03-01 09:16:42 +0000

    update text node implementation, add test
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@107709 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttools/renderers/LaTeX/tests/test_paragraph.py
+++ src/nti/contenttools/renderers/LaTeX/tests/test_paragraph.py
@@ -15,6 +15,7 @@ does_not = is_not
 from nti.contenttools.renderers.LaTeX.base import render_output
 
 from nti.contenttools.types.paragraph import Paragraph
+from nti.contenttools.types.text import TextNode
 
 from nti.contenttools.tests import ContentToolsTestCase
 
@@ -23,5 +24,12 @@ class TestParagraph(ContentToolsTestCase):
 
     def test_simple_par(self):
         par = Paragraph(element_type='manga')
+        output = render_output(par)
+        assert_that(output, is_(u'\n\n'))
+    
+    def test_par_with_text(self):
+        par = Paragraph(element_type='manga')
+        text = TextNode('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
+        par.add(text)
         output = render_output(par)
         assert_that(output, is_(u'\n\n'))
