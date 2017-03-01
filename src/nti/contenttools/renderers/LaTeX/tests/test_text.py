7908a7dd0f2df2c9af72f633c3f98fb2e0bb4f62
Egawati Panjei <egawati.panjei@nextthought.com>
2017-03-01 09:21:18 +0000

    add special char test on text node
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@107710 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/contenttools/renderers/LaTeX/tests/test_text.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
+
+from hamcrest import is_
+from hamcrest import is_not
+from hamcrest import assert_that
+does_not = is_not
+
+from nti.contenttools.renderers.LaTeX.base import render_output
+
+from nti.contenttools.types.text import TextNode
+
+from nti.contenttools.tests import ContentToolsTestCase
+
+class TestTextNode(ContentToolsTestCase):
+    
+    def test_simple_text_node(self):
+        node = TextNode('Hello There')
+        output = render_output(node)
+        assert_that(output, is_('Hello There'))
+    
+    def test_text_node_special_char(self):
+        node = TextNode(u'hello from plain µ δ Τ')
+        output = render_output(node)
+        assert_that(output, is_(u'hello from plain $\mu$ \u03b4 \u03a4'))
+        
