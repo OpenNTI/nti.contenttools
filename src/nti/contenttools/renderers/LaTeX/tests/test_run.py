f63996c29fc2fdd207121c69269e236e4feac898
Egawati Panjei <egawati.panjei@nextthought.com>
2017-03-01 10:13:57 +0000

    more test on run node renderer
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@107716 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttools/renderers/LaTeX/tests/test_run.py
+++ src/nti/contenttools/renderers/LaTeX/tests/test_run.py
@@ -39,51 +39,123 @@ class TestRunNode(ContentToolsTestCase):
         node.styles = ['bold']
         output = render_output(node)
         assert_that(output, is_(u'\\textbf{}'))
+    
+    def test_run_bold_with_text(self):
+        node = Run()
+        child = TextNode(u'doraemon')
+        node.add(child)
+        node.styles = ['bold']
+        output = render_output(node)
+        assert_that(output, is_(u'\\textbf{doraemon}'))
 
     def test_run_modified(self):
         node = Run()
         node.styles = ['inserted']
         output = render_output(node)
         assert_that(output, is_(u'\\modified{}'))
+    
+    def test_run_modified_with_text(self):
+        node = Run()
+        child = TextNode(u'doraemon')
+        node.add(child)
+        node.styles = ['inserted']
+        output = render_output(node)
+        assert_that(output, is_(u'\\modified{doraemon}'))
 
     def test_run_italic(self):
         node = Run()
         node.styles = ['italic']
         output = render_output(node)
         assert_that(output, is_(u'\\textit{}'))
+    
+    def test_run_italic_with_text(self):
+        node = Run()
+        node.styles = ['italic']
+        child = TextNode(u'doraemon')
+        node.add(child)
+        output = render_output(node)
+        assert_that(output, is_(u'\\textit{doraemon}'))
 
     def test_run_strike(self):
         node = Run()
         node.styles = ['strike']
         output = render_output(node)
         assert_that(output, is_(u'\\strikeout{}'))
+    
+    def test_run_strike_with_text(self):
+        node = Run()
+        node.styles = ['strike']
+        child = TextNode(u'doraemon')
+        node.add(child)
+        output = render_output(node)
+        assert_that(output, is_(u'\\strikeout{doraemon}'))
 
     def test_run_sub(self):
         node = Run()
         node.styles = ['sub']
         output = render_output(node)
         assert_that(output, is_(u'\\textsubscript{}'))
+    
+    def test_run_sub_with_text(self):
+        node = Run()
+        node.styles = ['sub']
+        child = TextNode(u'doraemon')
+        node.add(child)
+        output = render_output(node)
+        assert_that(output, is_(u'\\textsubscript{doraemon}'))
 
     def test_run_underline(self):
         node = Run()
         node.styles = ['underline']
         output = render_output(node)
         assert_that(output, is_(u'\\uline{}'))
+    
+    def test_run_underline_with_text(self):
+        node = Run()
+        node.styles = ['underline']
+        child = TextNode(u'doraemon')
+        node.add(child)
+        output = render_output(node)
+        assert_that(output, is_(u'\\uline{doraemon}'))
 
     def test_run_superscript(self):
         node = Run()
         node.styles = ['superscript']
         output = render_output(node)
         assert_that(output, is_(u'\\textsuperscript{}'))
+    
+    def test_run_superscript_with_text(self):
+        node = Run()
+        node.styles = ['superscript']
+        child = TextNode(u'doraemon')
+        node.add(child)
+        output = render_output(node)
+        assert_that(output, is_(u'\\textsuperscript{doraemon}'))
 
     def test_run_bold_underline(self):
         node = Run()
         node.styles = ['bold', 'underline']
         output = render_output(node)
         assert_that(output, is_(u'\\uline{\\textbf{}}'))
+    
+    def test_run_bold_underline_with_text(self):
+        node = Run()
+        node.styles = ['bold', 'underline']
+        child = TextNode(u'doraemon')
+        node.add(child)
+        output = render_output(node)
+        assert_that(output, is_(u'\\uline{\\textbf{doraemon}}'))
 
     def test_run_bold_underline_italic(self):
         node = Run()
         node.styles = ['bold', 'underline', 'italic']
         output = render_output(node)
         assert_that(output, is_(u'\\textit{\\uline{\\textbf{}}}'))
+    
+    def test_run_bold_underline_italic_with_text(self):
+        node = Run()
+        node.styles = ['bold', 'underline', 'italic']
+        child = TextNode(u'doraemon')
+        node.add(child)
+        output = render_output(node)
+        assert_that(output, is_(u'\\textit{\\uline{\\textbf{doraemon}}}'))
