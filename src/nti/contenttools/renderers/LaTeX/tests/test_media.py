2dade6dafb7e65113b5f590759f496cff049f8f2
Egawati Panjei <egawati.panjei@nextthought.com>
2017-03-02 07:34:25 +0000

    format code
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@107826 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttools/renderers/LaTeX/tests/test_media.py
+++ src/nti/contenttools/renderers/LaTeX/tests/test_media.py
@@ -22,12 +22,14 @@ from nti.contenttools.types.text import TextNode
 
 from nti.contenttools.tests import ContentToolsTestCase
 
+
 class TestMedia(ContentToolsTestCase):
+
     def test_empty_image(self):
         node = Image()
         output = render_output(node)
         assert_that(output, is_(u'\\includegraphics[width=0px,height=0px]{images/}'))
-    
+
     def test_annotation_image(self):
         node = Image()
         node.width = 500
@@ -35,8 +37,10 @@ class TestMedia(ContentToolsTestCase):
         node.predefined_image_path = True
         node.path = u'images/foo.png'
         output = render_output(node)
-        assert_that(output, is_('\\ntiincludeannotationgraphics[width=500px,height=450px]{images/foo.png}'))
-    
+        assert_that(
+            output,
+            is_('\\ntiincludeannotationgraphics[width=500px,height=450px]{images/foo.png}'))
+
     def test_noannotation_image(self):
         node = Image()
         node.annotation = False
@@ -45,8 +49,10 @@ class TestMedia(ContentToolsTestCase):
         node.predefined_image_path = True
         node.path = u'images/foo.png'
         output = render_output(node)
-        assert_that(output, is_(u'\\ntiincludenoannotationgraphics[width=500px,height=450px]{images/foo.png}'))
-    
+        assert_that(
+            output,
+            is_(u'\\ntiincludenoannotationgraphics[width=500px,height=450px]{images/foo.png}'))
+
     def test_inline_image(self):
         node = Image()
         node.inline_image = True
@@ -55,8 +61,10 @@ class TestMedia(ContentToolsTestCase):
         node.predefined_image_path = True
         node.path = u'images/foo.png'
         output = render_output(node)
-        assert_that(output, is_(u'\\includegraphics[width=30px,height=40px]{images/foo.png}'))
-    
+        assert_that(
+            output,
+            is_(u'\\includegraphics[width=30px,height=40px]{images/foo.png}'))
+
     def test_equation_image(self):
         node = Image()
         node.equation_image = True
@@ -65,33 +73,43 @@ class TestMedia(ContentToolsTestCase):
         node.predefined_image_path = True
         node.path = u'images/foo.png'
         output = render_output(node)
-        assert_that(output, is_(u'\\includegraphics[width=30px,height=40px]{images/foo.png}'))
-    
+        assert_that(
+            output,
+            is_(u'\\includegraphics[width=30px,height=40px]{images/foo.png}'))
+
     def test_simple_figure(self):
         figure = Figure()
         output = render_output(figure)
-        assert_that(output, is_(u'\\begin{figure}\n\\begin{center}\n\n\\end{center}\n\\end{figure}\n'))
-    
+        assert_that(
+            output,
+            is_(u'\\begin{figure}\n\\begin{center}\n\n\\end{center}\n\\end{figure}\n'))
+
     def test_figure_with_title(self):
         figure = Figure()
-        figure.title= TextNode(u'This is a figure title')
+        figure.title = TextNode(u'This is a figure title')
         output = render_output(figure)
-        assert_that(output, is_(u'\\begin{figure}\n\\begin{center}\n\\textbf{This is a figure title}\\\\\n\n\\end{center}\n\\end{figure}\n'))
-    
+        assert_that(
+            output,
+            is_(u'\\begin{figure}\n\\begin{center}\n\\textbf{This is a figure title}\\\\\n\n\\end{center}\n\\end{figure}\n'))
+
     def test_figure_with_caption(self):
         figure = Figure()
         figure.caption = u'This is figure caption'
         output = render_output(figure)
-        assert_that(output, is_(u'\\begin{figure}\n\\begin{center}\n\\caption{This is figure caption}\n\n\\end{center}\n\\end{figure}\n'))
-    
+        assert_that(
+            output,
+            is_(u'\\begin{figure}\n\\begin{center}\n\\caption{This is figure caption}\n\n\\end{center}\n\\end{figure}\n'))
+
     def test_figure_with_label(self):
         figure = Figure()
         figure.label = Run()
         child = TextNode(u'fig_label')
         figure.label.add(child)
         output = render_output(figure)
-        assert_that(output, is_(u'\\begin{figure}\n\\begin{center}\n\\label{fig_label}\n\\end{center}\n\\end{figure}\n'))
-    
+        assert_that(
+            output,
+            is_(u'\\begin{figure}\n\\begin{center}\n\\label{fig_label}\n\\end{center}\n\\end{figure}\n'))
+
     def test_figure_with_image(self):
         figure = Figure()
         img = Image()
@@ -101,11 +119,13 @@ class TestMedia(ContentToolsTestCase):
         img.path = u'images/foo.png'
         figure.add(img)
         output = render_output(figure)
-        assert_that(output, is_(u'\\begin{figure}\n\\begin{center}\n\\ntiincludeannotationgraphics[width=500px,height=450px]{images/foo.png}\n\\end{center}\n\\end{figure}\n'))
-    
+        assert_that(
+            output,
+            is_(u'\\begin{figure}\n\\begin{center}\n\\ntiincludeannotationgraphics[width=500px,height=450px]{images/foo.png}\n\\end{center}\n\\end{figure}\n'))
+
     def test_figure(self):
         figure = Figure()
-        figure.title= u'fig title'
+        figure.title = u'fig title'
         figure.caption = u'fig caption'
         figure.label = u'fig_label'
         img = Image()
@@ -115,5 +135,6 @@ class TestMedia(ContentToolsTestCase):
         img.path = u'images/foo.png'
         figure.add(img)
         output = render_output(figure)
-        assert_that(output, is_(u'\\begin{figure}\n\\begin{center}\n\\textbf{fig title}\\\\\n\\ntiincludeannotationgraphics[width=70px,height=90px]{images/foo.png}\\caption{fig caption}\n\\label{fig_label}\n\\end{center}\n\\end{figure}\n'))
-        
+        assert_that(
+            output,
+            is_(u'\\begin{figure}\n\\begin{center}\n\\textbf{fig title}\\\\\n\\ntiincludeannotationgraphics[width=70px,height=90px]{images/foo.png}\\caption{fig caption}\n\\label{fig_label}\n\\end{center}\n\\end{figure}\n'))
