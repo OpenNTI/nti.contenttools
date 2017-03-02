5f9233a74912964a488175d54944c436d9686661
Egawati Panjei <egawati.panjei@nextthought.com>
2017-03-02 07:08:44 +0000

    add more test on figure renderer
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@107822 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttools/renderers/LaTeX/tests/test_media.py
+++ src/nti/contenttools/renderers/LaTeX/tests/test_media.py
@@ -67,8 +67,39 @@ class TestMedia(ContentToolsTestCase):
         output = render_output(node)
         assert_that(output, is_(u'\\includegraphics[width=30px,height=40px]{images/foo.png}'))
     
-    def test_figure(self):
+    def test_simple_figure(self):
         figure = Figure()
         output = render_output(figure)
         assert_that(output, is_(u'\\begin{figure}\n\\begin{center}\n\n\\end{center}\n\\end{figure}\n'))
+    
+    def test_figure_with_title(self):
+        figure = Figure()
+        figure.title= TextNode(u'This is a figure title')
+        output = render_output(figure)
+        assert_that(output, is_(u'\\begin{figure}\n\\begin{center}\n\\textbf{This is a figure title}\\\\\n\n\\end{center}\n\\end{figure}\n'))
+    
+    def test_figure_with_caption(self):
+        figure = Figure()
+        figure.caption = u'This is figure caption'
+        output = render_output(figure)
+        assert_that(output, is_(u'\\begin{figure}\n\\begin{center}\n\\caption{This is figure caption}\n\n\\end{center}\n\\end{figure}\n'))
+    
+    def test_figure_with_label(self):
+        figure = Figure()
+        figure.label = Run()
+        child = TextNode(u'fig_label')
+        figure.label.add(child)
+        output = render_output(figure)
+        assert_that(output, is_(u'\\begin{figure}\n\\begin{center}\n\\label{fig_label}\n\\end{center}\n\\end{figure}\n'))
+    
+    def test_figure_with_image(self):
+        figure = Figure()
+        img = Image()
+        img.width = 500
+        img.height = 450
+        img.predefined_image_path = True
+        img.path = u'images/foo.png'
+        figure.add(img)
+        output = render_output(figure)
+        assert_that(output, is_(u'\\begin{figure}\n\\begin{center}\n\\ntiincludeannotationgraphics[width=500px,height=450px]{images/foo.png}\n\\end{center}\n\\end{figure}\n'))
         
