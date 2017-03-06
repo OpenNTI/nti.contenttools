159386376f8632fc9c421cac2fa2d13a0bb4a30b
Egawati Panjei <egawati.panjei@nextthought.com>
2017-03-06 12:48:05 +0000

    rewrite omath fraction, denominator and numerator tex renderer
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@108037 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttools/renderers/LaTeX/omath.py
+++ src/nti/contenttools/renderers/LaTeX/omath.py
@@ -21,6 +21,9 @@ from nti.contenttools.renderers.LaTeX.base import render_children_output
 from nti.contenttools.types.omath import IOMath
 from nti.contenttools.types.omath import IOMathRun
 from nti.contenttools.types.omath import IOMathPara
+from nti.contenttools.types.omath import IOMathFrac
+from nti.contenttools.types.omath import IOMathDenominator
+from nti.contenttools.types.omath import IOMathNumerator
 
 from nti.contenttools.renderers.interfaces import IRenderer
 
@@ -59,6 +62,47 @@ def render_omath_run(context, node):
     """
     return render_children(context, node)
 
+def render_omath_fraction(context, node):
+    """
+    render <m:f> element
+    """
+    if node.frac_type == u'lin':
+        context.write(u'{')
+        render_node(context, node.children[0])
+        context.write(u'}/{')
+        render_node(context, node.children[1])
+        context.write(u'}')
+    elif node.frac_type == u'skw':
+        context.write(u'{^{')
+        render_node(context, node.children[0])
+        context.write(u'}}/_{')
+        render_node(context, node.children[1])
+        context.write(u'}')
+    elif node.frac_type == u'noBar':
+        context.write(u'{')
+        render_node(context, node.children[0])
+        context.write(u' \\choose ')
+        render_node(context, node.children[1])
+    else:
+        context.write(u'\\frac{')
+        render_node(context, node.children[0])
+        context.write(u'}{')
+        render_node(context, node.children[1])
+        context.write(u'}')
+    return node
+
+def render_omath_numerator(context, node):
+    """
+    render <m:num>
+    """
+    return render_children(context, node)
+
+def render_omath_denominator(context, node):
+    """
+    render <m:den>
+    """
+    return render_children(context, node)
+
 @interface.implementer(IRenderer)
 class RendererMixin(object):
 
@@ -81,6 +125,18 @@ class OMathRenderer(RendererMixin):
 class OMathParaRenderer(RendererMixin):
     func = staticmethod(render_omath_para)
 
-@component.adapter(IOMathPara)
+@component.adapter(IOMathRun)
 class OMathRunRenderer(RendererMixin):
     func = staticmethod(render_omath_run)
+
+@component.adapter(IOMathFrac)
+class OMathFracRenderer(RendererMixin):
+    func = staticmethod(render_omath_fraction)
+
+@component.adapter(IOMathDenominator)
+class OMathDenominatorRenderer(RendererMixin):
+    func = staticmethod(render_omath_denominator)
+
+@component.adapter(IOMathNumerator)
+class OMathNumeratorRenderer(RendererMixin):
+    func = staticmethod(render_omath_numerator)
