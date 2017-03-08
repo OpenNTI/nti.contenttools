9411fb1eb72ec704169450704d1b17d483d676a4
Egawati Panjei <egawati.panjei@nextthought.com>
2017-03-08 09:45:50 +0000

    test for troubleshooting purpose
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@108250 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttools/renderers/LaTeX/tests/test_utils.py
+++ src/nti/contenttools/renderers/LaTeX/tests/test_utils.py
@@ -18,6 +18,8 @@ from nti.contenttools.renderers.LaTeX.utils import get_variant_field_string_valu
 from nti.contenttools.types.document import Document
 
 from nti.contenttools.types.interfaces import IItem
+from nti.contenttools.types.interfaces import IOrderedList
+from nti.contenttools.types.interfaces import IOMathMatrix
 
 from nti.contenttools.types.lists import Item
 from nti.contenttools.types.lists import OrderedList
@@ -27,6 +29,8 @@ from nti.contenttools.types.run import Run
 from nti.contenttools.types.text import TextNode
 
 from nti.contenttools.types.sectioning import Section
+
+from nti.contenttools.types.omath import OMathMatrix
 
 from nti.contenttools.tests import ContentToolsTestCase
 
@@ -58,6 +62,21 @@ class TestUtils(ContentToolsTestCase):
         root.add(section_2)
         result = search_node(IItem, root)
         assert_that(result, is_(False))
+        
+    def test_search_node_omatrix(self):
+        root = Document()
+        section_1 = Section()
+        omath_matrix = OMathMatrix()
+        section_1.add(omath_matrix)
+        section_2 = Section()
+        list_1 = OrderedList()
+        section_2.add(list_1)
+        root.add(section_1)
+        root.add(section_2)
+        result_list = search_node(IOrderedList, root)
+        assert_that(result_list, is_(True))
+        result = search_node(IOMathMatrix, root)
+        #assert_that(result, is_(True))
 
     def test_get_variant_field_string_value(self):
         node = Figure()
