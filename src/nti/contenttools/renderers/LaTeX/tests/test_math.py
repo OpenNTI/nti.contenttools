d68528b58d512ced5bccb19c70fb0ee7bd37922e
Egawati Panjei <egawati.panjei@nextthought.com>
2017-03-15 04:59:01 +0000

    check if <mover> should be rendered as \check{}, add test
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@108910 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttools/renderers/LaTeX/tests/test_math.py
+++ src/nti/contenttools/renderers/LaTeX/tests/test_math.py
@@ -1923,3 +1923,28 @@ class TestMath(ContentToolsTestCase):
         inline_math.equation_type = u'inline'
         output_inline = render_output(inline_math)
         assert_that(output_inline, is_(u'\\(\\breve{x}\\)'))
+    
+    def test_mover_check(self):
+        math = Math()
+        mrow = MRow()
+
+        mover_1 = MOver()
+        mover_1.accent = True
+        mi_1 = MathRun()
+        mi_1.element_type = 'identifier'
+        mi_1.add(TextNode(u'x', type_text=u'math'))
+        mover_1.add(mi_1)
+        mo_1 = MathRun()
+        mo_1.element_type = 'operator'
+        mo_1.add(TextNode(u'\u02C5', type_text=u'math'))
+        mover_1.add(mo_1)
+
+        mrow.add(mover_1)
+        math.add(mrow)
+        output = render_output(math)
+        assert_that(output, is_(u'\\[\\check{x}\\]'))
+
+        inline_math = math
+        inline_math.equation_type = u'inline'
+        output_inline = render_output(inline_math)
+        assert_that(output_inline, is_(u'\\(\\check{x}\\)'))
