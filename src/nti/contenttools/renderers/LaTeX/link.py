c628b17f4b733dcc0b22669efc73301edfab2c93
Egawati Panjei <egawati.panjei@nextthought.com>
2017-03-16 10:39:42 +0000

    rewrite hyperlink tex renderer
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@108985 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/contenttools/renderers/LaTeX/link.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id: note.py 108984 2017-03-16 10:13:56Z egawati.panjei $
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from zope import component
+from zope import interface
+
+from nti.contenttools.renderers.LaTeX.base import render_children
+
+from nti.contenttools.renderers.LaTeX.utils import get_variant_field_string_value
+
+from nti.contenttools.renderers.interfaces import IRenderer
+
+from nti.contenttools.types.interfaces import IHyperlink
+
+
+def render_hyperlink(context, node):
+    if not node.target:
+        render_children(context, node)
+    else:
+        target = get_variant_field_string_value(node.target)
+        if node.type == u'Normal' or node.type == u'Thumbnail':
+            context.write(u'\\href{')
+            context.write(target)
+            context.write(u'}{')
+            render_children(context, node)
+            context.write(u'}')
+        elif node.type == u'Youtube':
+            set_link(context, u'ntiincludevideo', target)
+        elif node.type == u'Pageref':
+            set_link(context, u'ntiidref', target)
+    return node
+
+def set_link(context, command, target):
+    context.write(u'\\')
+    context.write(command)
+    context.write(u'{')
+    context.write(target)
+    context.write(u'}')
