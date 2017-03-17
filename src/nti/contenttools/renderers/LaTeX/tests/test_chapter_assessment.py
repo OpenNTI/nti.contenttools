f0f6ba0f5ad5733f45055b1eeb2fae56257e0740
Egawati Panjei <egawati.panjei@nextthought.com>
2017-03-17 13:08:21 +0000

    add tests for chapter assessment nodes renderer
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@109087 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/contenttools/renderers/LaTeX/tests/test_chapter_assessment.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
+
+from hamcrest import is_
+from hamcrest import assert_that
+
+from nti.contenttools.renderers.LaTeX.base import render_output
+
+from nti.contenttools.types.chapter_assessment import ChapterExercise
+from nti.contenttools.types.chapter_assessment import ExerciseSection
+from nti.contenttools.types.chapter_assessment import ExerciseElement
+
+from nti.contenttools.types.run import Run
+from nti.contenttools.types.text import TextNode
+
+from nti.contenttools.tests import ContentToolsTestCase
+
+class TestChapterAssessment(ContentToolsTestCase):
+    def test_chapter_assessment(self):
+        node = ChapterExercise()
+        run = Run()
+        run.add(TextNode(u'Chapter Exercise'))
+        node.add(run)
+        output = render_output(node)
+        assert_that(output, u'Chapter Exercise')
+        
+    def test_exercise_section(self):
+        node = ExerciseSection()
+        run = Run()
+        run.add(TextNode(u'Exercise Section'))
+        node.add(run)
+        output = render_output(node)
+        assert_that(output, u'Exercise Section')
+    
+    def test_exercise_element(self):
+        node = ExerciseElement()
+        run = Run()
+        run.add(TextNode(u'Exercise Element'))
+        node.add(run)
+        output = render_output(node)
+        assert_that(output, u'Exercise Element')
