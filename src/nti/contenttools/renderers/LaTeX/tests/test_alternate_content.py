6ab7a0a0031ef03f811a9b83d9942f90d440c5ec
Egawati Panjei <egawati.panjei@nextthought.com>
2017-03-17 11:05:13 +0000

    register adapter, add tests for alternate content renderer
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@109079 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/contenttools/renderers/LaTeX/tests/test_alternate_content.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
+
+from hamcrest import is_
+from hamcrest import assert_that
+
+from nti.contenttools.renderers.LaTeX.base import render_output
+
+from nti.contenttools.types.alternate_content import AlternateContent
+from nti.contenttools.types.alternate_content import TextBoxContent
+
+from nti.contenttools.types.run import Run
+from nti.contenttools.types.text import TextNode
+
+from nti.contenttools.tests import ContentToolsTestCase
+
+class TestAlternateContent(ContentToolsTestCase):
+    
+    def test_alternate_content(self):
+        node = AlternateContent()
+        run = Run()
+        run.add(TextNode(u'This is an alternate content'))
+        node.add(run)
+        output = render_output(node)
+        assert_that(output, is_(u'This is an alternate content'))
+        
+    def test_text_box_content(self):
+        node = TextBoxContent()
+        run = Run()
+        run.add(TextNode(u'This is a text box'))
+        node.add(run)
+        output = render_output(node)
+        assert_that(output, u'\\parbox[c]{\\textwidth}{This is a text box}')
