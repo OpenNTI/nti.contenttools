2e4350778680e57dc8335e502fc718e4f2a71fec
Egawati Panjei <egawati.panjei@nextthought.com>
2017-03-27 09:30:35 +0000

    implement the docx table interfaces
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@109689 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/contenttools/types/docx_table.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id: docx_table.py 108562 2017-03-10 14:29:47Z carlos.sanchez $
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from zope import interface
+
+from nti.contenttools.types.interfaces import IDocxTable
+from nti.contenttools.types.interfaces import IDocxTRow
+from nti.contenttools.types.interfaces import IDocxTCell
+
+from nti.contenttools.types.node import DocumentStructureNode
+
+from nti.schema.fieldproperty import createFieldProperties
+
+
+@interface.implementer(IDocxTable)
+class DocxTable(DocumentStructureNode):
+    createFieldProperties(IDocxTable)
+
+    def __init__(self, *args, **kwargs):
+        super(DocxTable, self).__init__(*args, **kwargs)
+
+@interface.implementer(IDocxTRow)
+class DocxTRow(DocumentStructureNode):
+    createFieldProperties(IDocxTRow)
+
+    def __init__(self, *args, **kwargs):
+        super(DocxTRow, self).__init__(*args, **kwargs)
+
+@interface.implementer(IDocxTCell)
+class DocxTCell(DocumentStructureNode):
+    createFieldProperties(IDocxTRow)
+
+    def __init__(self, *args, **kwargs):
+        super(DocxTCell, self).__init__(*args, **kwargs)
