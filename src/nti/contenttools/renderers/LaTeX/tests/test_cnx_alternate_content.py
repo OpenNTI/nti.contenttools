00adbe42f8c50fb9a7296613195cfd07751e1969
Egawati Panjei <egawati.panjei@nextthought.com>
2017-03-27 07:24:09 +0000

    create test for cnx alternate content renderer
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@109686 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/contenttools/renderers/LaTeX/tests/test_cnx_alternate_content.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+from nti.contenttools import cnx
+__docformat__ = "restructuredtext en"
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
+
+from hamcrest import is_
+from hamcrest import assert_that
+
+from nti.contenttools.renderers.LaTeX.base import render_output
+
+from nti.contenttools.types.cnx import CNXProblemSolution
+
+
+from nti.contenttools.types.run import Run
+from nti.contenttools.types.text import TextNode
+
+from nti.contenttools.tests import ContentToolsTestCase
+
+
+class TestCNXAlternateContent(ContentToolsTestCase):
+    def test_cnx_problem_solution(self):
+        node = CNXProblemSolution()
+        node.title = TextNode(u'Title')
+        run_1 = Run()
+        run_1.add(TextNode(u'CNX Problem\n'))
+        node.add(run_1)
+        run_2 = Run()
+        run_2.add(TextNode(u'CNX Solution'))
+        node.add(run_2)
+        
+        output = render_output(node)
+        assert_that(output, is_(u'\\textbf{Title}\\\\\nCNX Problem CNX Solution'))
