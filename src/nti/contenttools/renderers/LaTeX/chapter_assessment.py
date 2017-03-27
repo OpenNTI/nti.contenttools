3501f02a262a68783ed2c3a0615af436f6fec65c
Egawati Panjei <egawati.panjei@nextthought.com>
2017-03-27 07:30:18 +0000

    format code, cleanup
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@109687 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttools/renderers/LaTeX/chapter_assessment.py
+++ src/nti/contenttools/renderers/LaTeX/chapter_assessment.py
@@ -5,7 +5,7 @@
 """
 
 from __future__ import print_function, unicode_literals, absolute_import, division
-from nti.contenttools.renders.LaTeX.chapter_assessment import set_solution_tag
+
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
