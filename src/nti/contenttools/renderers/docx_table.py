49e9d07bd41bcd24d318633cabf9f3f81c7aee7c
Egawati Panjei <egawati.panjei@nextthought.com>
2017-03-27 13:00:10 +0000

    rewrite docx table cell tex renderer
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@109695 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttools/renderers/docx_table.py
+++ src/nti/contenttools/renderers/docx_table.py
@@ -5,12 +5,25 @@
 """
 
 from __future__ import print_function, unicode_literals, absolute_import, division
+from docutils.nodes import colspec
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+from zope import component
+from zope import interface
 
+from nti.contenttools.renderers.interfaces import IRenderer
+
+from nti.contenttools.renderers.LaTeX.base import render_children
 from nti.contenttools.renderers.LaTeX.base import render_node
+
+from nti.contenttools.renderers.LaTeX.utils import create_label
+from nti.contenttools.renderers.LaTeX.utils import get_variant_field_string_value
+
+from nti.contenttools.types.interfaces import IDocxTable
+from nti.contenttools.types.interfaces import IDocxTRow
+from nti.contenttools.types.interfaces import IDocxTCell
 
 
 def render_docx_table(context, node):
@@ -25,27 +38,25 @@ def render_docx_table(context, node):
 
 def get_colspec(node):
     colspec = u''
-    if      'start' in node.borders.keys() \
-        and node.borders['start']['val'] not in ['nil', 'none']:
+    if 'start' in node.borders.keys() and node.borders['start'][
+            'val'] not in ['nil', 'none']:
         colspec = u'|'
-    elif 'left' in node.borders.keys() \
-            and node.borders['left']['val'] not in ['nil', 'none']:
+    elif 'left' in node.borders.keys() and node.borders['left']['val'] not in ['nil', 'none']:
         colspec = u'|'
 
     alignment = get_alignment(node)
     for _ in xrange(len(node.grid) - 1):
         colspec = colspec + alignment
-        if      'insideV' in node.borders.keys() \
-            and node.borders['insideV']['val'] not in ['nil', 'none']:
+        if 'insideV' in node.borders.keys() and node.borders['insideV'][
+                'val'] not in ['nil', 'none']:
             colspec = colspec + u'|'
-    if      'end' in node.borders.keys() \
-        and node.borders['end']['val'] not in ['nil', 'none']:
+    if 'end' in node.borders.keys() and node.borders['end']['val'] not in ['nil', 'none']:
         colspec = colspec + alignment + u'|'
-    elif    'right' in node.borders.keys() \
-        and node.borders['right']['val'] not in ['nil', 'none']:
+    elif 'right' in node.borders.keys() and node.borders['right']['val'] not in ['nil', 'none']:
         colspec = colspec + alignment + u'|'
     else:
         colspec = colspec + alignment
+
     return colspec
 
 
@@ -65,18 +76,63 @@ def get_alignment(node):
 
 
 def get_table_body(context, node):
-    if      'top' in node.borders.keys() \
-        and node.borders['top']['val'] not in ['nil', 'none']:
+    if 'top' in node.borders.keys() and node.borders['top']['val'] not in ['nil', 'none']:
         context.write(u'\\hline\n')
 
     for child in node.children:
         render_node(context, child)
-        if      'insideH' in node.borders.keys() \
-            and node.borders['insideH']['val'] not in ['nil', 'none']:
+        if 'insideH' in node.borders.keys() and node.borders['insideH'][
+                'val'] not in ['nil', 'none']:
             context.write(u'\\hline\n')
 
-    if      'bottom' in node.borders.keys() \
-        and node.borders['bottom']['val'] not in ['nil', 'none']:
+    if 'bottom' in node.borders.keys() and node.borders['bottom'][
+            'val'] not in ['nil', 'none']:
         context.write(u'\\hline\n')
 
     return node
+
+def render_docx_trow(context, node):
+    check = len(node.children) - 1
+    for index, child in enumerate(node.children):
+        render_node(context, child)
+        if index < check:
+            context.write(u' & ')
+    context.write(u'\\\\\n')
+    return node
+
+def render_docx_tcell(context, node):
+    if node.grid_span > 1:
+        context.write(u'\\multicolumn{')
+        context.write(node.grid_span)
+        context.write(u'}{c}{')
+        render_children(context, node)
+        context.write(u'}')
+    else:
+        render_children(context, node)
+    return node
+
+@interface.implementer(IRenderer)
+class RendererMixin(object):
+
+    func = None
+
+    def __init__(self, node):
+        self.node = node
+
+    def render(self, context, node=None):
+        node = self.node if node is None else node
+        return self.func(context, node)
+    __call__ = render
+
+
+@component.adapter(IDocxTable)
+class DocxTableRenderer(RendererMixin):
+    func = staticmethod(render_docx_table)
+
+@component.adapter(IDocxTRow)
+class DocxTRowRenderer(RendererMixin):
+    func = staticmethod(render_docx_trow)
+
+@component.adapter(IDocxTCell)
+class DocxTCellRenderer(RendererMixin):
+    func = staticmethod(render_docx_tcell)
