501d2c8866208ac6f5cf1cfe2e47b3d535301ad5
Egawati Panjei <egawati.panjei@nextthought.com>
2017-04-10 07:22:25 +0000

    rewrite the adapters, start from epub ifsta
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@110438 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/contenttools/adapters/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id: __init__.py 105585 2017-02-01 02:54:15Z carlos.sanchez $
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
---
+++ src/nti/contenttools/adapters/epub/ifsta/lists.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id: lists.py 106706 2017-02-16 01:07:01Z carlos.sanchez $
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from nti.contenttools import types
+
+from nti.contenttools.adapters.epub.ifsta import check_child
+from nti.contenttools.adapters.epub.ifsta import check_element_text
+from nti.contenttools.adapters.epub.ifsta import check_element_tail
+
+class OrderedList( types.OrderedList ):
+    @classmethod
+    def process(cls, element):
+        me = cls()
+        if 'data-number-style' in element.attrib:
+            numbering_type = element.attrib['data-number-style']
+            me.start = 1
+
+            if numbering_type == u'1': me.format = 'decimal'
+            elif u'lower-alpha' in  numbering_type : me.format = 'lowerLetter'
+            elif u'upper-alpha' in numbering_type : me.format = 'upperLetter'
+            elif u'lower-roman' in numbering_type : me.format = 'lowerRoman'
+            elif u'upper-roman' in numbering_type : me.format = 'upperRoman'
+            elif u'arabic' in numbering_type : me.format = 'decimal'
+            else:
+                logger.warn("UNHANDLED OrderedList numbering format type %s", numbering_type)
+
+        for child in element:
+            el = None
+            if child.tag == 'li':
+                el = Item.process(child)
+            else:
+                logger.info('OrderedList child %s',child.tag)
+                el = Item()
+            if isinstance(el, types.Item) or isinstance(el, types.List):
+                me.add_child( el )
+            else:
+                if len(me.children) == 0:
+                    me.add_child( Item() )
+                me.children[-1].add_child( el )
+        return me
+
+class UnorderedList( types.UnorderedList ):
+    @classmethod
+    def process(cls, element):
+        me = cls()
+        if 'style' in element.attrib:
+            numbering_style = element.attrib['style']
+            me.start = 1
+            if u'circle' in numbering_style: me.format = u'circ'
+            elif u'disc' in numbering_style: me.format = u'bullet'
+            elif u'square' in numbering_style: me.format = u'blacksquare'
+            else:
+                logger.warn("UNHANDLED UnorderedList numbering format type %s", numbering_style)
+        for child in element:
+            el = None
+            if child.tag == 'li':
+                el = Item.process(child, format=me.format)
+            elif child.tag == 'div':
+                from nti.contenttools.adapters.epub.ifsta.run import process_div_elements
+                el = process_div_elements(child, me)
+            elif child.tag == 'p':
+                from nti.contenttools.adapters.epub.ifsta.paragraph import Paragraph
+                el = Paragraph.process(child)
+            else:
+                el = Item()
+
+            if isinstance(el, types.Item) or isinstance(el, types.List):
+                me.add_child( el )
+            else:
+                if len(me.children) == 0:
+                    me.add_child( Item() )
+                me.children[-1].add_child( el )
+        return me
+
+class Item( types.Item ):
+    @classmethod
+    def process(cls, element, format=None):
+        me = cls()
+        me = check_element_text(me, element)
+        me = check_child(me, element)
+        me = check_element_tail(me, element)
+        me.bullet_type = format
+        return me
