592f9d91a4dd0a0a8d9a2e8414f0709a4be649e5
Egawati Panjei <egawati.panjei@nextthought.com>
2017-04-18 11:53:10 +0000

    format code
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@110897 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttools/parser/epub_to_latex.py
+++ src/nti/contenttools/parser/epub_to_latex.py
@@ -13,58 +13,64 @@ import os
 import logging
 import argparse
 
-from nti.contenttools import scoped_registry
-
 from nti.contenttools.epub_parser import EPUBParser
 
 from nti.contenttools.util import string_replacer
 
 DEFAULT_FORMAT_STRING = '[%(asctime)-15s] [%(name)s] %(levelname)s: %(message)s'
 
+
 def _parse_args():
-	arg_parser = argparse.ArgumentParser(description="NTI EPUB-Latex Parser")
-	arg_parser.add_argument('inputfile',
-							 help="The epub filepath")
-	arg_parser.add_argument('-o', '--output',
-							 default='output',
-							 help="The output directory. The default is: %s" % 'output')
-	arg_parser.add_argument('-t', '--type',
-							 default='generic',
-							 help="The epub type. The default is: %s" % 'generic')
-	return arg_parser.parse_args()
+    arg_parser = argparse.ArgumentParser(description="NTI EPUB-Latex Parser")
+    arg_parser.add_argument('inputfile',
+                            help="The epub filepath")
+    arg_parser.add_argument('-o', '--output',
+                            default='output',
+                            help="The output directory. The default is: %s" % 'output')
+    arg_parser.add_argument('-t', '--type',
+                            default='generic',
+                            help="The epub type. The default is: %s" % 'generic')
+    return arg_parser.parse_args()
+
 
 def _title_escape(title):
-	return string_replacer.rename_filename(title)
+    return string_replacer.rename_filename(title)
+
 
 def _configure_logging(level='INFO'):
-	numeric_level = getattr(logging, level.upper(), None)
-	numeric_level = logging.INFO if not isinstance(numeric_level, int) else numeric_level
-	logging.basicConfig(level=numeric_level)
+    numeric_level = getattr(logging, level.upper(), None)
+    numeric_level = logging.INFO if not isinstance(numeric_level, int) else numeric_level
+    logging.basicConfig(level=numeric_level)
+
 
 def _setup_configs():
-	_configure_logging()
+    _configure_logging()
+
 
 def main():
-	# Parse command line args
-	args = _parse_args()
+    # Parse command line args
+    args = _parse_args()
 
-	_setup_configs()
+    _setup_configs()
 
-	inputfile = os.path.expanduser(args.inputfile)
+    inputfile = os.path.expanduser(args.inputfile)
 
-	# Verify the input file exists
-	if not os.path.exists(inputfile):
-		logger.info('The source file, %s, does not exist.', inputfile)
-		exit()
+    # Verify the input file exists
+    if not os.path.exists(inputfile):
+        logger.info('The source file, %s, does not exist.', inputfile)
+        exit()
 
-	# Create the output directory if it does not exist
-	if not os.path.exists(args.output):
-		os.mkdir(args.output)
+    # Create the output directory if it does not exist
+    if not os.path.exists(args.output):
+        os.mkdir(args.output)
 
-	# create a txt file to store information about image's name and location used in nticard
-	scoped_registry.nticard_images_filename = os.path.join(args.output, 'nticard_images.txt')
-	epub = EPUBParser(inputfile, args.output, args.type)
-	epub.process_fragment()
+    # create a txt file to store information about image's name and location
+    # used in nticard
+    epub = EPUBParser(inputfile, args.output, args.type)
+    epub.nticard_images_filename = os.path.join(
+        args.output,
+        'nticard_images.txt')
+    epub.process_fragment()
 
 if __name__ == '__main__':  # pragma: no cover
-	main()
+    main()
