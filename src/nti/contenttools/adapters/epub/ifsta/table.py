a9080870c845c58bd00dcc0764a702131057db6a
Egawati Panjei <egawati.panjei@nextthought.com>
2017-04-18 04:10:24 +0000

    adapter for table
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@110870 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/contenttools/adapters/epub/ifsta/table.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id: table.py 110445 2017-04-10 13:34:47Z carlos.sanchez $
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from nti.contenttools import types
+
+from lxml.html import HtmlComment
+
+from nti.contenttools.adapters.epub.ifsta.run import Run
+
+from nti.contenttools.adapters.epub.ifsta import check_child
+from nti.contenttools.adapters.epub.ifsta import check_element_text
+
+class Table(types.Table):
+    @classmethod
+    def process(cls, element):
+        me = cls()
+        if 'id' in element.attrib : 
+            me.label = element.attrib['id']
+        
+        me = check_element_text(me, element)
+        
+        if u'border' in element.attrib.keys():
+            me.border = element.attrib[u'border']
+
+        for child in element:
+            if child.tag == 'colgroup':
+                pass
+            elif child.tag == 'tbody':
+                if me.border:
+                    me.add_child(TBody.process(child, True))
+                else:
+                    me.add_child(TBody.process(child, False))
+            elif child.tag == 'tr':
+                me.add_child(Row.process(child, me.border))
+            elif child.tag == 'thead':
+                me.add_child(THead.process(child, me.border))
+            elif child.tag == 'tfoot':
+                me.add_child(TFoot.process(child))
+            elif child.tag == 'caption':
+                caption = Run.process(child)
+                me.caption = caption
+            else:
+                if not isinstance(child, HtmlComment):
+                    logger.warn('Unhandled %s child: %s.', element, child)
+        return me
+
+
+class TBody(types.TBody):
+    @classmethod
+    def process(cls, element, border=None):
+        me = cls()
+        me.border = border
+        number_of_col = 0 
+        count_child = -1
+        me = check_element_text(me, element)
+        for child in element:
+            if child.tag == 'tr':
+                if me.border:
+                    me.add_child(Row.process(child, me.border))
+                else:
+                    me.add_child(Row.process(child))
+                number_of_col = me.children[count_child].number_of_col
+                count_child = count_child + 1
+            else:
+                if isinstance(child,HtmlComment):
+                    pass
+                else:
+                    logger.warn('Unhandled <tbody> child: %s.',child.tag)
+        me.number_of_col = number_of_col
+        return me
+
+class THead(types.THead):  
+    @classmethod
+    def process(cls, element, border=None):
+        me = cls()
+        me.border = border
+        number_of_col = 0 
+        count_child = -1
+        for child in element:
+            if child.tag == 'tr':
+                me.add_child(Row.process(child, border))
+                number_of_col = me.children[count_child].number_of_col
+                count_child = count_child + 1
+            else:
+                if isinstance(child,HtmlComment):
+                    pass
+                else:
+                    logger.warn('Unhandled <thead> child: %s.',child.tag)
+        me.number_of_col = number_of_col
+        return me
+
+class TFoot(types.TFoot):
+    @classmethod
+    def process(cls, element):
+        me = cls()
+        number_of_col = 0 
+        count_child = -1
+        for child in element:
+            if child.tag == 'tr':
+                me.add_child(Row.process(child))
+                number_of_col = me.children[count_child].number_of_col
+                count_child = count_child + 1
+            else:
+                if isinstance(child,HtmlComment): pass
+                else:
+                    logger.warn('Unhandled <tfoot> child: %s.',child.tag)
+        me.number_of_col = number_of_col
+        return me
+
+class Row (types.Row):
+    @classmethod
+    def process(cls, element, border=None):
+        me = cls()
+        me.border = border
+        number_of_col = 0
+        me = check_element_text(me, element)
+        for child in element:
+            if child.tag == 'td' or child.tag == 'th':
+                me.add_child(Cell.process(child, border))
+                if number_of_col == 0:
+                    me.children[0].is_first_cell_in_the_row = True
+                number_of_col += 1
+            else:
+                if isinstance(child,HtmlComment):
+                    pass
+                else:
+                    logger.warn('Unhandled <tr> child: %s.',child.tag)
+        me.number_of_col = number_of_col
+        return me
+
+class Cell(types.Cell):
+    @classmethod
+    def process(cls, element, border = None):
+        me = cls()
+        me.border = border
+        
+        if u'valign' in element.attrib.keys():
+            me.v_alignment = element.attrib[u'valign']
+
+        if u'colspan' in element.attrib.keys():
+            me.colspan = int(element.attrib[u'colspan'])
+
+        if u'style' in element.attrib.keys():
+            style = element.attrib[u'style']
+            if u'text-align' in style:
+                idx = style.find(u'text-align')
+                text_align = style[idx:style.find(u';')]
+                me.h_alignment = text_align[text_align.find(u':')+1:].strip()
+
+        me = check_element_text(me, element)
+        me = check_child(me, element)
+        return me
