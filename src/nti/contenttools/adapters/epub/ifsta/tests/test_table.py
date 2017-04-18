aa347a97dd15551a1fddfc2ddfb2c9f3ae53e506
Egawati Panjei <egawati.panjei@nextthought.com>
2017-04-18 06:05:32 +0000

    test table
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@110880 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/contenttools/adapters/epub/ifsta/tests/test_table.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
+
+from hamcrest import is_
+from hamcrest import is_not
+from hamcrest import assert_that
+does_not = is_not
+
+from lxml import html
+
+from nti.contenttools.adapters.epub.ifsta.run import Run
+
+from nti.contenttools.renderers.LaTeX.base import render_output
+
+from nti.contenttools.tests import ContentToolsTestCase
+
+class TestTable(ContentToolsTestCase):
+    def test_table(self):
+        script = u'<div><table>\
+        <tr>\
+        <td>A</td>\
+        <td>B</td>\
+        <td>C</td>\
+        </tr>\
+        <tr>\
+        <td>1</td>\
+        <td>2</td>\
+        <td>3</td>\
+        </tr></table></div>'
+        element = html.fromstring(script)
+        node = Run.process(element)
+        output = render_output(node)
+        assert_that(output,
+                    is_(u'\n\\begin{table}\n\\begin{tabular}{}\nA & B & C\\\\\n1 & 2 & 3\\\\\n\n\\end{tabular}\n\\end{table}\n'))
