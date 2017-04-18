f28a8aadc48c568bc993b0420bf35e01d51e0f9d
Egawati Panjei <egawati.panjei@nextthought.com>
2017-04-18 11:07:24 +0000

    clean up
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@110891 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttools/adapters/epub/__init__.py
+++ src/nti/contenttools/adapters/epub/__init__.py
@@ -24,13 +24,13 @@ from nti.contenttools import types
 
 from nti.contenttools.renderers.LaTeX.base import render_output
 
-from .. import scoped_registry
 
 class EPUBParser(object):
+
     def __init__(self, input_file, output_directory, epub_type):
         self.image_list = []
         self.latex_filenames = []
-        self.content_folder = [] #will be use to retrieve images or pdf
+        self.content_folder = []  # will be use to retrieve images or pdf
         self.epub_type = epub_type
 
         self.input_file = input_file
@@ -38,9 +38,9 @@ class EPUBParser(object):
         self.tex_filepath = []
 
         self.epub_reader = EPUBReader(input_file)
-        main_title = rename_filename (self.epub_reader.title)
+        main_title = rename_filename(self.epub_reader.title)
         self.book_title = main_title
-        self.tex_main_file = u'MAIN_%s.tex' %main_title
+        self.tex_main_file = u'MAIN_%s.tex' % main_title
 
     def process_fragment(self):
         epub_reader = self.epub_reader
@@ -53,59 +53,65 @@ class EPUBParser(object):
                 epub_chapter = adapt_ifsta(fragment, self)
             else:
                 pass
-                #TODO create generic adapter
+                # TODO create generic adapter
                 #epub_chapter = adapt(fragment)
-            tex_filename = u'%s.tex' %rename_filename(item)
+            tex_filename = u'%s.tex' % rename_filename(item)
             self.latex_filenames.append(tex_filename)
             tex_content = render_output(epub_chapter)
-            self.write_to_file(tex_content,tex_filename)
+            self.write_to_file(tex_content, tex_filename)
         self.create_main_latex()
         logger.info(epub_reader.spine)
 
     def create_main_latex(self):
-        main_tex_content = generate_main_tex_content(self.epub_reader.metadata, self.latex_filenames)
+        main_tex_content = generate_main_tex_content(
+            self.epub_reader.metadata,
+            self.latex_filenames)
         self.write_to_file(main_tex_content, self.tex_main_file)
-        
-    def write_to_file(self, content, filename, type_= None):
-        if type_ is None : 
-            filepath = u'%s/%s' %(self.output_directory, filename)
-            self.tex_filepath.append(filepath) 
-            with codecs.open(filepath,'w', 'utf-8') as file_:
+
+    def write_to_file(self, content, filename, type_=None):
+        if type_ is None:
+            filepath = u'%s/%s' % (self.output_directory, filename)
+            self.tex_filepath.append(filepath)
+            with codecs.open(filepath, 'w', 'utf-8') as file_:
                 file_.write(content)
 
+
 def get_packages():
-    LATEX_PACKAGES = [u'graphix', 
-                 u'hyperref', 
-                 u'ulem', 
-                 u'Tabbing', 
-                 u'textgreek', 
-                 u'amsmath', 
-                 u'nticourse', 
-                 u'ntilatexmacros', 
-                 u'ntiassessment',
-                 u'ntislidedeck',
-                 u'ntiglossary',
-                 ]
+    LATEX_PACKAGES = [u'graphix',
+                      u'hyperref',
+                      u'ulem',
+                      u'Tabbing',
+                      u'textgreek',
+                      u'amsmath',
+                      u'nticourse',
+                      u'ntilatexmacros',
+                      u'ntiassessment',
+                      u'ntislidedeck',
+                      u'ntiglossary',
+                      ]
     package_list = []
     package_list_append = package_list.append
     for package in LATEX_PACKAGES:
-        string = u'\\usepackage{%s}\n' %(package)
+        string = u'\\usepackage{%s}\n' % (package)
         package_list_append(string)
     return u''.join(package_list)
+
 
 def get_included_tex(included_tex_list):
     result = []
     result_append = result.append
     for tex in included_tex_list:
-        inc = u'\\include{%s}\n' %(tex)
+        inc = u'\\include{%s}\n' % (tex)
         result_append(inc)
     return u''.join(result)
 
+
 def generate_main_tex_content(metadata, included_tex_list):
-    title = u'\\title{%s}\n' %metadata[u'title'] if 'title' in metadata else u''
+    title = u'\\title{%s}\n' % metadata[u'title'] if 'title' in metadata else u''
     author = u''
     if u'creator' in metadata.keys():
-        author = u'\\author{%s}\n' %metadata['creator']
+        author = u'\\author{%s}\n' % metadata['creator']
     package = get_packages()
     latex = get_included_tex(included_tex_list)
-    return u'\\documentclass{book}\n%s%s%s\\begin{document}\n%s\\end{document}' %(package, title, author, latex)
+    return u'\\documentclass{book}\n%s%s%s\\begin{document}\n%s\\end{document}' % (
+        package, title, author, latex)
