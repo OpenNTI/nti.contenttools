528b5281f650d7c8edfbcf05ec82d6fa57e33001
Egawati Panjei <egawati.panjei@nextthought.com>
2017-04-18 10:15:40 +0000

    continue cleaning up
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@110888 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttools/adapters/epub/ifsta/media.py
+++ src/nti/contenttools/adapters/epub/ifsta/media.py
@@ -9,8 +9,7 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-from nti.contenttools import types 
-
+from nti.contenttools import types
 
 from PIL import Image as PILImage
 
@@ -20,24 +19,21 @@ try:
     import cStringIO as StringIO
 except:
     import StringIO
-    
+
 from nti.contenttools.adapters.epub.ifsta.run import Run
 from nti.contenttools.adapters.epub.ifsta.run import process_div_elements
 
-from nti.contenttools.adapters.epub.ifsta import check_child
-from nti.contenttools.adapters.epub.ifsta import check_element_text
-
-
 class Image(types.Image):
+
     @classmethod
-    def process(cls, element,inline_image=False, epub=None):
+    def process(cls, element, inline_image=False, epub=None):
         me = cls()
         path = element.attrib['src']
-        if u'../' in path :
+        if u'../' in path:
             path = path.replace('../', '')
         head, filename = os.path.split(path)
         me.predefined_image_path = True
-        me.path = u'Images/CourseAssets/%s/%s' %(epub.book_title, filename)
+        me.path = u'Images/CourseAssets/%s/%s' % (epub.book_title, filename)
         me.inline_image = inline_image
         if 'alt' in element.attrib.keys():
             me.caption = types.TextNode(element.attrib['alt'])
@@ -50,31 +46,38 @@ class Image(types.Image):
             save_image(image_data, me.path, epub)
             me.width, me.height = PILImage.open(image_data).size
         else:
-            logger.warn('COULD NOT FIND Image : %s',image_path) 
+            logger.warn('COULD NOT FIND Image : %s', image_path)
             return types.Run()
 
-        return me    
+        return me
+
 
 def save_image(image_data, filepath, epub):
     filepath = u'%s/%s' % (epub.output_directory, filepath)
     if not os.path.exists(os.path.dirname(filepath)):
         os.makedirs(os.path.dirname(filepath))
 
-    with open( filepath, 'wb' ) as file:
+    with open(filepath, 'wb') as file:
         file.write(image_data.read())
 
+
 class Figure(types.Figure):
+
     @classmethod
     def process(cls, element, epub=None):
         me = cls()
-        if u'id' in element.attrib : me.label = element.attrib[u'id']
+        if u'id' in element.attrib:
+            me.label = element.attrib[u'id']
         for child in element:
             if child.tag == u'figcaption':
                 me.caption = Run.process(child, epub=epub)
             elif child.tag == u'span':
-                if u'data-type' in child.attrib : me.data_type = child.attrib[u'data-type']
-                if u'id' in child.attrib : me.image_id = child.attrib[u'id']
-                if u'data-alt' in child.attrib : me.image_alt = types.TextNode(child.attrib[u'data-alt'])
+                if u'data-type' in child.attrib:
+                    me.data_type = child.attrib[u'data-type']
+                if u'id' in child.attrib:
+                    me.image_id = child.attrib[u'id']
+                if u'data-alt' in child.attrib:
+                    me.image_alt = types.TextNode(child.attrib[u'data-alt'])
                 img = get_figure_image(child, epub)
                 me.add_child(img)
             elif child.tag == u'figure':
@@ -84,6 +87,7 @@ class Figure(types.Figure):
             else:
                 logger.warn('Unhandled figure child %s', child.tag)
         return me
+
 
 def get_figure_image(element, epub=None):
     for child in element:
