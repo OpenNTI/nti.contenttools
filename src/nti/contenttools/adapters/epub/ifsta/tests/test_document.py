b1b3121c5801e9cd26ee2843d556b044bb27b7a2
Egawati Panjei <egawati.panjei@nextthought.com>
2017-04-21 09:21:35 +0000

    clean up
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@111092 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttools/adapters/epub/ifsta/tests/test_document.py
+++ src/nti/contenttools/adapters/epub/ifsta/tests/test_document.py
@@ -7,8 +7,6 @@ __docformat__ = "restructuredtext en"
 # disable: accessing protected members, too many methods
 # pylint: disable=W0212,R0904
 
-logger = __import__('logging').getLogger(__name__)
-
 from hamcrest import is_
 from hamcrest import is_not
 from hamcrest import assert_that
@@ -20,8 +18,6 @@ from nti.testing.matchers import validly_provides
 from nti.testing.matchers import verifiably_provides
 
 from nti.contenttools.types.interfaces import IEPUBBody
-
-from nti.contenttools.adapters.epub.ifsta.paragraph import Paragraph
 
 from nti.contenttools.adapters.epub.ifsta import EPUBBody
 
