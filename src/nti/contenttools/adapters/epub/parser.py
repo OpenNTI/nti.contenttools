374bf830f0e0f5d26d00f51be10cf86d4839852d
Egawati Panjei <egawati.panjei@nextthought.com>
2017-04-24 11:58:40 +0000

    clean up
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@111252 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttools/adapters/epub/parser.py
+++ src/nti/contenttools/adapters/epub/parser.py
@@ -5,7 +5,7 @@
 """
 
 from __future__ import print_function, unicode_literals, absolute_import, division
-from docutils.nodes import section
+
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
