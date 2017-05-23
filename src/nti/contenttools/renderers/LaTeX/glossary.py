4968a7b478cf6257d0936020434e0641b7f16a0e
Egawati Panjei <egawati.panjei@nextthought.com>
2017-05-23 08:44:47 +0000

    render glossary entry, add test
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@113375 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/contenttools/renderers/LaTeX/glossary.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id: footnote.py 107525 2017-02-27 09:43:34Z egawati.panjei $
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+import six
+
+from zope import component
+from zope import interface
+
+from nti.contenttools.renderers.interfaces import IRenderer
+
+from nti.contenttools.types.interfaces import IGlossaryEntry
+
+from nti.contenttools.renderers.LaTeX.utils import get_variant_field_string_value
+
+def render_glossary_entry(context, node):
+    term  = get_variant_field_string_value(node.term)
+    definition = get_variant_field_string_value(node.definition)
+    context.write(u'\\ntiglossaryentry{')
+    context.write(term)
+    context.write(u'}{')
+    context.write(definition)
+    context.write(u'}')
+    return node
+
+@interface.implementer(IRenderer)
+class RendererMixin(object):
+
+    func = None
+
+    def __init__(self, node):
+        self.node = node
+
+    def render(self, context, node=None):
+        node = self.node if node is None else node
+        return self.func(context, node)
+    __call__ = render
+
+
+@component.adapter(IGlossaryEntry)
+class GlossaryEntryRenderer(RendererMixin):
+    func = staticmethod(render_glossary_entry)
---
+++ src/nti/contenttools/renderers/LaTeX/tests/test_glossary.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
+
+from hamcrest import is_
+from hamcrest import assert_that
+
+from nti.contenttools.renderers.LaTeX.base import render_output
+
+from nti.contenttools.types.glossary import GlossaryEntry
+
+from nti.contenttools.types.text import TextNode
+
+from nti.contenttools.tests import ContentToolsTestCase
+
+class TestGlossary(ContentToolsTestCase):
+    def test_glossary_entry(self):
+        node = GlossaryEntry()
+        
+        term = TextNode(u'term')
+        definition = TextNode(u'definition')
+        
+        node.term = term
+        node.definition = definition
+        
+        output = render_output(node)
+        
+        assert_that(output,is_(u'\\ntiglossaryentry{term}{definition}'))
+        
