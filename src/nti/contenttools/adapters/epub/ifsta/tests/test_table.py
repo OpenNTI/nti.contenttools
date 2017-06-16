f17eac4026c2ee20612f19054ae422bf5ce62df2
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-06-16 16:16:09 +0000

    fix tests
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@115276 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttools/adapters/epub/ifsta/tests/test_table.py
+++ src/nti/contenttools/adapters/epub/ifsta/tests/test_table.py
@@ -24,17 +24,17 @@ from nti.contenttools.tests import ContentToolsTestCase
 class TestTable(ContentToolsTestCase):
 
     def test_table(self):
-        script = u'<div><table>\
-        <tr>\
-        <td>A</td>\
-        <td>B</td>\
-        <td>C</td>\
-        </tr>\
-        <tr>\
-        <td>1</td>\
-        <td>2</td>\
-        <td>3</td>\
-        </tr></table></div>'
+        script = u"""<div><table>
+        <tr>
+        <td>A</td>
+        <td>B</td>
+        <td>C</td>
+        </tr>
+        <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+        </tr></table></div>"""
         element = html.fromstring(script)
         node = Run.process(element)
         output = render_output(node)
@@ -42,23 +42,23 @@ class TestTable(ContentToolsTestCase):
                     is_(u'\n\\begin{table}\n\\begin{tabular}{|l|l|l|}\nA & B & C\\\\ \\hline\n1 & 2 & 3\\\\ \\hline\n\n\\end{tabular}\n\\end{table}\n'))
 
     def test_table_2(self):
-        script = u'<div><table>\
-        <tr>\
-        <th>Firstname</th>\
-        <th>Lastname</th>\
-        <th>Age</th>\
-        </tr>\
-        <tr>\
-        <td>Jill</td>\
-        <td>Smith</td>\
-        <td>50</td>\
-        </tr>\
-        <tr>\
-        <td>Eve</td>\
-        <td>Jackson</td>\
-        <td>94</td>\
-        </tr>\
-        </table></div>'
+        script = u"""<div><table>
+        <tr>
+        <th>Firstname</th>
+        <th>Lastname</th>
+        <th>Age</th>
+        </tr>
+        <tr>
+        <td>Jill</td>
+        <td>Smith</td>
+        <td>50</td>
+        </tr>
+        <tr>
+        <td>Eve</td>
+        <td>Jackson</td>
+        <td>94</td>
+        </tr>
+        </table></div>"""
         element = html.fromstring(script)
         node = Run.process(element)
         output = render_output(node)
