756792fdebd64a5e29d6a43c16d4f6c3ba9779f8
Egawati Panjei <egawati.panjei@nextthought.com>
2017-06-27 08:00:24 +0000

    fix typo
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@116002 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttools/parser/epub_to_latex.py
+++ src/nti/contenttools/parser/epub_to_latex.py
@@ -85,7 +85,7 @@ def main():
 
     # create a txt file to store information about image's name and location
     # used in nticard
-    epub = EPUBParser(inputfile, args.output, args.type, args.cssjson, arg.chapternum)
+    epub = EPUBParser(inputfile, args.output, args.type, args.cssjson, args.chapternum)
     epub.nticard_images_filename = os.path.join(args.output,
                                                 'nticard_images.txt')
     epub.process_fragment()
