38da1019f765fc02de45aae557e4c0d8ce4832ab
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-08-22 15:07:30 +0000

    pep8; set props;
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@120232 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttools/renderers/LaTeX/glossary.py
+++ src/nti/contenttools/renderers/LaTeX/glossary.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-.. $Id: footnote.py 107525 2017-02-27 09:43:34Z egawati.panjei $
+.. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import, division
+from __future__ import print_function, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
