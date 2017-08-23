782c5f26e746cbc18d50001ffc4a0189126a770e
Egawati Panjei <egawati.panjei@nextthought.com>
2017-08-23 15:52:11 +0000

    add test for html parser
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@120321 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttools/adapters/html/mathcounts/parser.py
+++ src/nti/contenttools/adapters/html/mathcounts/parser.py
@@ -5,13 +5,14 @@
 """
 
 from __future__ import print_function, absolute_import, division
+
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
 from lxml import html
 
-from os.path import basename
+from os.path import splitext
 
 from nti.contenttools.adapters.html.mathcounts.run import HTMLBody
 
@@ -30,7 +31,7 @@ def adapt(fragment, html):
 
 class MathcountsHTMLParser(object):
 
-    def __init__(self, script, output_dir, tex_filename=None):
+    def __init__(self, script, output_dir, tex_filename):
         self.script = script
         self.output_dir = output_dir
         self.naq_counter = 0
@@ -40,14 +41,13 @@ class MathcountsHTMLParser(object):
         self.cleanup_filename()
         element = html.fromstring(self.script)
         node = adapt(element, self)
-        context = DefaultRendererContext(name="LaTeX")
-        render_node(context, node)
-        _ = context.read()
+        self.context = DefaultRendererContext(name=u"LaTeX")
+        render_node(self.context, node)
 
     def cleanup_filename(self):
         self.tex_filename = rename_filename(self.tex_filename)
         if '.tex' in self.tex_filename:
-            self.labelling = basename(self.tex_filename)
+            self.labelling, _ = splitext(self.tex_filename)
         else:
             self.labelling = self.tex_filename
             self.tex_filename = '%s.tex' % self.tex_filename
