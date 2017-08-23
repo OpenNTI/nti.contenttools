93ae1dd5387e513da43fc77fb7359061e1624e53
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-08-23 17:00:25 +0000

    clean up
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttools/trunk@120336 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttools/adapters/html/mathcounts/tests/test_parser.py
+++ src/nti/contenttools/adapters/html/mathcounts/tests/test_parser.py
@@ -16,16 +16,15 @@ from lxml import html
 
 from nti.contenttools.adapters.html.mathcounts.parser import MathcountsHTMLParser
 
-from nti.contenttools.renderers.LaTeX.base import render_output
-
 from nti.contenttools.tests import ContentToolsTestCase
 
 
 class TestMathcountsHTMLParser(ContentToolsTestCase):
-	def test_symmath(self):
-		script = u'<html><body><div><p class="Normal ParaOverride-4"><span class="CharOverride-5">1.  </span><span class="CharOverride-6">In a standard deck of 52 playing cards, the red number cards greater than 6 are the 7, 8, 9 and 10 in the suits of diamonds and hearts. That’s a total of 8 cards. The percent probability that Perta randomly selects one of these 8 cards, then, is 8/52 </span><span class="CharOverride-7">≈</span><span class="CharOverride-6"> </span><span class="CharOverride-5">15.38</span><span class="CharOverride-6">%.</span></p></div></body></html>'
-		element = html.fromstring(script)
-		parser = MathcountsHTMLParser(script, 'output_dir', 'test.tex')
-		parser.process()
-		assert_that(parser.context.read(),
-		        is_("\\begin{document}\n\\begin{naquestion}[individual=true]\n\\label{naqsymmath:test_1}\\begin{naqsymmathpart}\n1. In a standard deck of 52 playing cards, the red number cards greater than 6 are the 7, 8, 9 and 10 in the suits of diamonds and hearts. That's a total of 8 cards. The percent probability that Perta randomly selects one of these 8 cards, then, is 8/52 $\\approx$ 15.38\\%.\n\n\n\\begin{naqsolutions}\n\n\\end{naqsolutions}\n\n\\begin{naqsolexplanation}\n\n\\end{naqsolexplanation}\n\n\n\\end{naqsymmathpart}\n\n\\end{naquestion}\n\n\\end{document}\n"))
+
+    def test_symmath(self):
+        script = u'<html><body><div><p class="Normal ParaOverride-4"><span class="CharOverride-5">1.  </span><span class="CharOverride-6">In a standard deck of 52 playing cards, the red number cards greater than 6 are the 7, 8, 9 and 10 in the suits of diamonds and hearts. That’s a total of 8 cards. The percent probability that Perta randomly selects one of these 8 cards, then, is 8/52 </span><span class="CharOverride-7">≈</span><span class="CharOverride-6"> </span><span class="CharOverride-5">15.38</span><span class="CharOverride-6">%.</span></p></div></body></html>'
+        _ = html.fromstring(script)
+        parser = MathcountsHTMLParser(script, 'output_dir', 'test.tex')
+        parser.process()
+        assert_that(parser.context.read(),
+                    is_("\\begin{document}\n\\begin{naquestion}[individual=true]\n\\label{naqsymmath:test_1}\\begin{naqsymmathpart}\n1. In a standard deck of 52 playing cards, the red number cards greater than 6 are the 7, 8, 9 and 10 in the suits of diamonds and hearts. That's a total of 8 cards. The percent probability that Perta randomly selects one of these 8 cards, then, is 8/52 $\\approx$ 15.38\\%.\n\n\n\\begin{naqsolutions}\n\n\\end{naqsolutions}\n\n\\begin{naqsolexplanation}\n\n\\end{naqsolexplanation}\n\n\n\\end{naqsymmathpart}\n\n\\end{naquestion}\n\n\\end{document}\n"))
