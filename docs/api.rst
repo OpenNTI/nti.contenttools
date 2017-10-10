794314044e4c63f6d657413ab3bec62d51379cc0
Carlos Sanchez <papachoco@gmail.com>
2017-10-09 22:13:54 -0500

    add docs  and supporting files

---
+++ docs/api.rst
@@ -0,0 +1,63 @@
+===========
+ Reference
+===========
+
+Compat
+======
+
+.. automodule:: nti.contenttools._compat
+
+Glossary JSON Exporter
+======================
+
+.. automodule:: nti.contenttools.glossary_json_exporter
+
+Glossary Term Finder
+====================
+
+.. automodule:: nti.contenttools.glossary_term_finder
+
+Import EPUB
+===========
+
+.. automodule:: nti.contenttools.import_epub
+
+Interfaces
+==========
+
+.. automodule:: nti.contenttools.interfaces
+
+LaTeX
+=====
+
+.. automodule:: nti.contenttools.latex
+
+Parse HTML LaTeX
+================
+
+.. automodule:: nti.contenttools.parse_html_latex
+
+Parse MathCounts HTML LaTeX
+===========================
+
+.. automodule:: nti.contenttools.parse_mathcounts_html_latex
+
+Tag Parser
+==========
+
+.. automodule:: nti.contenttools.tag_parser
+
+Tag Parser
+==========
+
+.. automodule:: nti.contenttools.tag_parser
+
+Unicode to LaTeX
+================
+
+.. automodule:: nti.contenttools.unicode_to_latex
+
+Word to LaTeX
+=============
+
+.. automodule:: nti.contenttools.word2latex
--- setup.py
+++ setup.py
@@ -1,13 +1,6 @@
 import codecs
 from setuptools import setup, find_packages
 
-VERSION = '0.0.0'
-
-TESTS_REQUIRE = [
-    'nti.testing',
-    'zope.testrunner',
-]
-
 entry_points = {
     "z3c.autoinclude.plugin": [
         'target = nti.contenttools',
@@ -24,26 +17,44 @@ entry_points = {
     ]
 }
 
+TESTS_REQUIRE = [
+    'nti.testing',
+    'zope.testrunner',
+]
+
+
+def _read(fname):
+    with codecs.open(fname, encoding='utf-8') as f:
+        return f.read()
+
+
 setup(
     name='nti.contenttools',
-    version=VERSION,
+    version=_read('version.txt').strip(),
     author='Jason Madden',
     author_email='jason@nextthought.com',
-    description="NTI Content Tools",
-    long_description=codecs.open('README.rst', encoding='utf-8').read(),
-    license='Proprietary',
-    keywords='Content tools',
+    description="NTI contentt tools",
+    long_description=(_read('README.rst') + '\n\n' + _read("CHANGES.rst")),
+    license='Apache',
+    keywords='Base',
     classifiers=[
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
     ],
+    url="https://github.com/NextThought/nti.contenttools",
+    zip_safe=True,
     packages=find_packages('src'),
     package_dir={'': 'src'},
+    include_package_data=True,
     namespace_packages=['nti'],
+    tests_require=TESTS_REQUIRE,
     install_requires=[
         'setuptools',
         'lxml',
@@ -63,6 +74,11 @@ setup(
     ],
     extras_require={
         'test': TESTS_REQUIRE,
+        'docs': [
+            'Sphinx',
+            'repoze.sphinx.autointerface',
+            'sphinx_rtd_theme',
+        ],
     },
     entry_points=entry_points
 )
