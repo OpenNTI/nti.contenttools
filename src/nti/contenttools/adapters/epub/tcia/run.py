1a35df31e3c4ac107518a7c9531d74a4afc67759
Egawati Panjei <egawati.panjei@gmail.com>
2018-05-22 17:30:31 -0500

    cleanup tcia adapter, use things from generic

--- src/nti/contenttools/adapters/epub/tcia/run.py
+++ src/nti/contenttools/adapters/epub/tcia/run.py
@@ -15,28 +15,9 @@ from nti.contenttools.adapters.epub.tcia import check_child
 from nti.contenttools.adapters.epub.tcia import check_element_text
 from nti.contenttools.adapters.epub.tcia import check_element_tail
 
+from nti.contenttools.adapters.epub.generic.run import Run
+
 from nti.contenttools.types import TextNode
-
-
-class Run(types.Run):
-
-    @classmethod
-    def process(cls, element, styles=(), reading_type=None, epub=None):
-        me = cls()
-        me.styles.extend(styles)
-        me = check_element_text(me, element)
-        me = check_child(me, element, epub)
-        if element.tail:
-            _t = cls()
-            _t.add_child(me)
-            tail = element.tail.replace('\r', '').replace('\t', '')
-            _t.add_child(types.TextNode(tail))
-            me = _t
-        return me
-
-def process_div_elements(element, parent, epub=None):
-    el = Run.process(element, epub=epub)
-    return el
 
 def process_span_elements(element, epub=None):
     el = Run.process(element, epub=epub)
