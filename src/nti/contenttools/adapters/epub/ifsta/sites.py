df747651a3f6cde9dfb5528e86576ec260ad182b
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-05-21 22:06:57 -0500

    add ifsta components

---
+++ src/nti/contenttools/adapters/epub/ifsta/sites.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+from z3c.baseregistry.baseregistry import BaseComponents
+
+from nti.contenttools.adapters.epub.generic.sites import GENERIC
+
+IFSTA = BaseComponents(GENERIC,
+                       name='ifsta',
+                       bases=(GENERIC,))
