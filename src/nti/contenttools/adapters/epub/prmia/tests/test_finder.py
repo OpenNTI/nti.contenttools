1a66c3b4bde7923e1ae5c107cdc68f35b701c38b
Egawati Panjei <egawati.panjei@gmail.com>
2018-06-06 14:06:05 -0500

    update find_ref_node test

--- src/nti/contenttools/adapters/epub/prmia/tests/test_finder.py
+++ src/nti/contenttools/adapters/epub/prmia/tests/test_finder.py
@@ -24,9 +24,29 @@ from nti.contenttools.adapters.epub.prmia.tests import create_epub_object
 
 from nti.contenttools.adapters.epub.prmia.finder import find_ref_node
 
-class TestRunAdapter(PRMIATestCase):
-    def test_div_group(self):
+class TestFinder(PRMIATestCase):
+    def test_find_ref_node(self):
         script = u"""<div><p class="footnote"><sup><a id="ch03fn28"></a><a href="ch03.html#ch03fn_28">28</a></sup>Federal Reserve Chairman Ben S. Bernanke said &#8220;the Fed plans to avert strains in the banking system by pushing financial companies to better manage liquidity risk and reduce reliance on wholesale funding. Regulators will continue to press banks to reduce further their dependence on wholesale funding, which proved highly unreliable during the crisis.&#8221; Speech given on April 8, 2013, in Stone Mountain, Georgia.</p></div>"""
+        element = html.fromstring(script)
+        epub = create_epub_object()
+        node = Run.process(element, epub=epub)
+        ref_label_node = []
+        ref_label_node = find_ref_node(node, ref_label_node)
+        assert_that(ref_label_node[0],
+                    is_('ch03fn_28'))
+
+    def test_find_ref_node2(self):
+        script = u"""<div><sup><a id="ch03fn28"></a><a href="ch03.html#ch03fn_28">28</a></sup></div>"""
+        element = html.fromstring(script)
+        epub = create_epub_object()
+        node = Run.process(element, epub=epub)
+        ref_label_node = []
+        ref_label_node = find_ref_node(node, ref_label_node)
+        assert_that(ref_label_node[0],
+                    is_('ch03fn_28'))
+
+    def test_find_ref_node3(self):
+        script = u"""<div><a href="ch03.html#ch03fn_28">28</a></div>"""
         element = html.fromstring(script)
         epub = create_epub_object()
         node = Run.process(element, epub=epub)
