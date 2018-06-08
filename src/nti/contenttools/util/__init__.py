be5c81a8d6b5492982bf894f0141f3af0f8708e6
Egawati Panjei <egawati.panjei@gmail.com>
2018-06-08 14:47:05 -0500

    a function to merge dict

--- src/nti/contenttools/util/__init__.py
+++ src/nti/contenttools/util/__init__.py
@@ -11,3 +11,9 @@ logger = __import__('logging').getLogger(__name__)
 
 from .string_replacer import rename_filename
 from .string_replacer import create_label
+
+
+def merge_two_dicts(x, y):
+	z = x.copy()
+	z.update(y)
+	return z
