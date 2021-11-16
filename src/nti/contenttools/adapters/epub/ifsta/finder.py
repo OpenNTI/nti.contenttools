3bb3b459ce12c986d60d6d65c09c461b12c00d05
Sean Jones <sean@thehouseofjones.net>
2021-11-16 09:59:07 -0600

    This portion needs localization

--- src/nti/contenttools/adapters/epub/ifsta/finder.py
+++ src/nti/contenttools/adapters/epub/ifsta/finder.py
@@ -228,7 +228,7 @@ def search_and_update_figure_caption_reflowable(root, captions, figures, figure_
             caps = [cap for cap in captions if old_cap in cap]
             if caps:
                 new_cap = caps[0]
-                token = u'Figure %s' % (old_cap)
+                token = u'Imagen %s' % (old_cap)
                 new_cap = new_cap.replace(token.rstrip(), u'')
                 root.caption = new_cap.rstrip()
                 figures.append(root)
